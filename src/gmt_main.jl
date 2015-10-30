aafa5403d41f6850172088a2769a983faa8b646b
Joaquim Luis <jmfluis@gmail.com>
2015-10-30 01:34:07 +0000

    Improve contents of error message

--- src/gmt_main.jl
+++ src/gmt_main.jl
@@ -767,7 +767,7 @@ function GMTJL_dataset_init(API::Ptr{Void}, ptr, direction::Integer)
 		elseif (eltype(ptr) == UInt8)		Mb._type = UInt32(GMT.GMT_UCHAR)
 		elseif (eltype(ptr) == Int8)		Mb._type = UInt32(GMT.GMT_CHAR)
 		else
-			error("GMTJL_matrix_init: only floating point types allowed in input. Others need to be added")
+			error("GMTJL_matrix_init: only integer or floating point types allowed in input. Others need to be added")
 		end
 		Mb.data = pointer(ptr)
 		Mb.dim  = Mb.n_rows		# Data from Julia is in column major
