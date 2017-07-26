48ffccb9469cb58bcc5cc17b826622a157255242
Joaquim Luis <jmfluis@gmail.com>
2017-07-26 11:05:55 +0100

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,7 @@ using Base.Test
 @test 1 == 1
 r = gmt("gmtinfo -C",ones(Float32,9,3)*5);
 assert(r[1].data == [5.0 5 5 5 5 5])
+#
+G=gmt("grdmath -R0/10/0/10 -I1 5");
+r=gmt("grdinfo -C", G);
+assert(r[1].data == [0.0  10.0  0.0  10.0  5.0  5.0  1.0  1.0  11.0  11.0])
