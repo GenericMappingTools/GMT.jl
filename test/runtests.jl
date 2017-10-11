9cdad0554db84f04df91a0f18ad58ecb5a759a00
Joaquim Luis <jmfluis@gmail.com>
2017-10-11 20:07:20 +0100

    Add a grdcontour test

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,7 @@ assert(r[1].data == [0.0  10.0  0.0  10.0  5.0  5.0  1.0  1.0  11.0  11.0])
 #
 cpt = makecpt(range="-1/1/0.1");
 assert((size(cpt.colormap,1) == 20) && (cpt.colormap[1,:] == [0.875, 0.0, 1.0]))
+#
+G = gmt("grdmath -R-15/15/-15/15 -I0.3 X Y HYPOT DUP 2 MUL PI MUL 8 DIV COS EXCH NEG 10 DIV EXP MUL =");
+C = grdcontour(G, C="+0.7", D=[]);
+assert((size(C[1].data,1) == 21) && norm(-0.6 - C[1].data[1,1]) < 1e-8)
