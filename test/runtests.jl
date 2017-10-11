6aeca1c751f69634591c02518c2eaf2a40165b89
Joaquim Luis <jmfluis@gmail.com>
2017-10-11 12:08:07 +0100

    Add makecpt test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,6 @@ assert(r[1].data == [5.0 5 5 5 5 5])
 G=gmt("grdmath -R0/10/0/10 -I1 5");
 r=gmt("grdinfo -C", G);
 assert(r[1].data == [0.0  10.0  0.0  10.0  5.0  5.0  1.0  1.0  11.0  11.0])
+#
+cpt = makecpt(range="-1/1/0.1");
+assert((size(cpt.colormap,1) == 20) && (cpt.colormap[1,:] == [0.875, 0.0, 1.0]))
