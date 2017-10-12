21b599395633cc831394855e1d849ea99cc0dd17
Joaquim Luis <jmfluis@gmail.com>
2017-10-12 02:47:47 +0100

    Add grimage & grdview commands

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,3 +16,7 @@ assert((size(cpt.colormap,1) == 20) && (cpt.colormap[1,:] == [0.875, 0.0, 1.0]))
 G = gmt("grdmath -R-15/15/-15/15 -I0.3 X Y HYPOT DUP 2 MUL PI MUL 8 DIV COS EXCH NEG 10 DIV EXP MUL =");
 C = grdcontour(G, C="+0.7", D=[]);
 assert((size(C[1].data,1) == 21) && norm(-0.6 - C[1].data[1,1]) < 1e-8)
+#
+# Just create the figs but not check if they are correct.
+grdimage(G, J="X10", fmt="ps")
+grdview(G, J="X6i", JZ=5,  Q="s", C="topo", R="-15/15/-15/15/-1/1", view="120/30", fmt="ps")
