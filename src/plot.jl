8268cbd14cc8d50a2654c4e711d5211d1a04a65a
Joaquim Luis <jmfluis@gmail.com>
2017-11-09 14:46:02 +0000

    Use qualified calls to 'xy'

--- src/plot.jl
+++ src/plot.jl
@@ -82,29 +82,29 @@ Parameters
 # -----------------------------------------------------------------------------------------------------
 # Tested with plot(xyz, S="c0.1c", C=cpt, fmt="ps", show=1)
 plot(arg1::Array; extra="", data=[], fmt="", K=false, O=false, first=true, kw...) =
-xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=K, O=O, first=first, kw...)
+    GMT.xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=K, O=O, first=first, kw...)
 plot!(arg1::Array; extra="", data=[], fmt="", K=false, O=false, first=true, kw...) =
-xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=true, O=true, first=false, kw...)
+    GMT.xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=true, O=true, first=false, kw...)
 
 # -----------------------------------------------------------------------------------------------------
 plot(arg1::GMTdataset; extra="", data=[], fmt="", K=false, O=false, first=true, kw...) =
-	xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=K, O=O, first=first, kw...)
+	GMT.xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=K, O=O, first=first, kw...)
 plot!(arg1::GMTdataset; extra="", data=[], fmt="", K=false, O=false, first=true, kw...) =
-	xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=true, O=true, first=false, kw...)
+	GMT.xy(extra, arg1; caller="plot", data=data, fmt=fmt, K=true, O=true, first=false, kw...)
 
 # ------------------------------------------------------------------------------------------------------
 plot(arg1::String; extra="", data=[], fmt="", K=false, O=false, first=true, kw...) =
-	xy(extra, []; caller="plot", data=arg1, fmt=fmt, K=K, O=O, first=first, kw...)
+	GMT.xy(extra, []; caller="plot", data=arg1, fmt=fmt, K=K, O=O, first=first, kw...)
 plot!(arg1::String; extra="", data=[], fmt="", K=false, O=false, first=true, kw...) =
-	xy(extra, []; caller="plot", data=arg1, fmt=fmt, K=true, O=true, first=false, kw...)
+	GMT.xy(extra, []; caller="plot", data=arg1, fmt=fmt, K=true, O=true, first=false, kw...)
 
 # ------------------------------------------------------------------------------------------------------
 function plot(arg1::Array, arg2::Array; extra="", data=[], fmt="", K=false, O=false, first=true, kw...)
 	arg = hcat(arg1, arg2)
-	xy(extra, arg; caller="plot", data=[], fmt=fmt, K=K, O=O, first=first, kw...)
+	GMT.xy(extra, arg; caller="plot", data=[], fmt=fmt, K=K, O=O, first=first, kw...)
 end
 function plot!(arg1::Array, arg2::Array; extra="", data=[], fmt="", K=false, O=false, first=true, kw...)
 	arg = hcat(arg1, arg2)
-	xy(extra, arg; caller="plot", data=[], fmt=fmt, K=true, O=true, first=false, kw...)
+	GMT.xy(extra, arg; caller="plot", data=[], fmt=fmt, K=true, O=true, first=false, kw...)
 end
 # ------------------------------------------------------------------------------------------------------
