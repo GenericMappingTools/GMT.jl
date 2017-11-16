a1b07192596fa2fd7db3f6c1395fb68d9e88557d
Joaquim Luis <jmfluis@gmail.com>
2017-11-16 00:40:30 +0000

    Improvements

--- src/psconvert.jl
+++ src/psconvert.jl
@@ -37,7 +37,8 @@ Parameters
     The listfile is an ASCII file with the names of the PostScript files to be converted.
     [`-L`](http://gmt.soest.hawaii.edu/doc/latest/psconvert.html#l)
 - **Q** : **anti_aliasing** : -- Str --
-    Set the anti-aliasing options for graphics or text. Append the size of the subsample box (1, 2, or 4) [4].
+	Set the anti-aliasing options for graphics or text. Append the size of the subsample box
+	(1, 2, or 4) [4]. This option is set by default.
     [`-Q`](http://gmt.soest.hawaii.edu/doc/latest/psconvert.html#q)
 - **S** : **gs_command** : -- Bool or [] --
     Print to standard error the GhostScript command after it has been executed.
@@ -45,7 +46,8 @@ Parameters
 - **T** : **format** : -- Str --
     b|e|E|f|F|j|g|G|m|s|t Sets the output format, where b = BMP, e = EPS, E = EPS with PageSize command,
     f = PDF, F = multi-page PDF, j = JPEG, g = PNG, G = transparent PNG (untouched regions are
-    transparent), m = PPM,  and t = TIFF [default is JPEG].
+	transparent), m = PPM,  and t = TIFF [default is JPEG].
+	Alternatively, the format may be set with the *fmt* keyword, e.g. *fmt="png"*.
     [`-T`](http://gmt.soest.hawaii.edu/doc/latest/psconvert.html#t)
 - **W** : **world_file** : -- Str --
     Write a ESRI type world file suitable to make (e.g) .tif files be recognized as geotiff by
@@ -62,16 +64,18 @@ Parameters
 # ---------------------------------------------------------------------------------------------------
 function psconvert(cmd0::String="", arg1=[]; data=[], fmt::String="", kwargs...)
 
-	length(kwargs) == 0 && isempty_(data) && return monolitic("psconvert", cmd0, arg1)	# Speedy mode
+	length(kwargs) == 0 && isempty_(data) && contains(cmd0, " -") &&
+		return monolitic("psconvert", cmd0, arg1)	# Speedy mode
 
 	if (!isempty_(data) && !isa(data, String))
 		error("When using 'data', it MUST contain a String data type (the file name")
 	end
 
-	d = KW(kwargs)
-	cmd = parse_V("", d)
+	if (!isempty(cmd0)) && isempty_(arg1)  arg1 = cmd0  end
 
-	cmd = add_opt(cmd, 'A', d, [:A :adjust])
+	d = KW(kwargs)
+	cmd = add_opt("", 'A', d, [:A :adjust])
+	if (cmd == " -A")  cmd = cmd * "1p"  end		# If just -A default to -A1p
 	cmd = add_opt(cmd, 'D', d, [:D :out_dir :output_dir])
 	cmd = add_opt(cmd, 'E', d, [:E :dpi])
 	cmd = add_opt(cmd, 'F', d, [:F :out_name :output_name])
@@ -82,6 +86,16 @@ function psconvert(cmd0::String="", arg1=[]; data=[], fmt::String="", kwargs...)
 	cmd = add_opt(cmd, 'S', d, [:S :gs_command])
 	cmd = add_opt(cmd, 'T', d, [:T :format])
 	cmd = add_opt(cmd, 'Z', d, [:Z :del_input_ps])
+	cmd = parse_V(cmd, d)
+
+	if (!contains(cmd, "-T") && !isempty(fmt) && fmt != "ps")	# Must convert the FMT into a -T opt
+		if (fmt == "pdf")      cmd = cmd * " -Tf"
+		elseif (fmt == "eps")  cmd = cmd * " -Te"
+		elseif (fmt == "png")  cmd = cmd * " -Tg"
+		elseif (fmt == "jpg")  cmd = cmd * " -Tj"
+		elseif (fmt == "tif")  cmd = cmd * " -Tt"
+		end
+	end
 
 	for sym in [:C :gs_option]
 		if (haskey(d, sym))
@@ -101,7 +115,7 @@ function psconvert(cmd0::String="", arg1=[]; data=[], fmt::String="", kwargs...)
 		cmd = cmd * " -W+k" * d[:kml]
 	end
 
-	got_output = false
+	want_output = false
 	if (haskey(d, :in_memory))
 		cmd = cmd * " ="
 		if (!isempty_(arg1))
@@ -109,21 +123,37 @@ function psconvert(cmd0::String="", arg1=[]; data=[], fmt::String="", kwargs...)
 			      Dropping this one.")
 			arg1 = []
 		end
-		got_output = true
+		want_output = true
 	end
 
 	# In case DATA holds a file name, copy it into cmd.
 	cmd, arg1, = read_data(data, cmd, arg1)
 
+	if (isempty_(arg1))
+		error("Must provide one input PS file name or GMTps type struct")
+	end
+
+	if (isempty(cmd))          cmd = "-A1p -Tj -Qg4 -Qt4"  end 	# Means no options were used. Allowed case
+	if (!contains(cmd, "-Q"))  cmd = cmd * " -Qt4 -Qg4"    end	# We promised to have these as default
+	if (isa(arg1, String))
+		cmd = cmd * " " * arg1
+		is_struct_PS = false
+	elseif (isa(arg1, GMTps))
+		is_struct_PS = true
+	else
+		error("Input mut either be a STRING (file name) or a GMTps type (a struct with the PS contents)")
+	end
+
 	(haskey(d, :Vd)) && println(@sprintf("\tpsconvert %s", cmd))
 
-	if (got_output)
-		if (!isempty_(arg1))  return gmt("psconvert " * cmd, arg1)
-		else                  return gmt("psconvert " * cmd)
+	if (want_output)
+		if (is_struct_PS)  return gmt("psconvert " * cmd, arg1)
+		else               return gmt("psconvert " * cmd)
 		end
 	else
-		if (!isempty_(arg1))  gmt("psconvert " * cmd, arg1)
-		else                  gmt("psconvert " * cmd)
+		if (is_struct_PS)  gmt("psconvert " * cmd, arg1)
+		else               gmt("psconvert " * cmd)
 		end
+		return nothing
 	end
 end
