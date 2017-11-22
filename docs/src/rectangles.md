f3e2cd669f627bde372fb047c9c819828545abaf
Joaquim Luis <jmfluis@gmail.com>
2017-11-22 01:21:30 +0000

    Examples of rectangle drawing

---
+++ docs/src/rectangles.md
@@ -0,0 +1,41 @@
+# Draw rectangles
+
+## Simple filled rectangle
+
+```julia
+using GMT
+rect = [2 2; 2 6; 6 6; 6 2; 2 2];
+plot(rect, region=[0 10 0 10], lw=1, fill="blue", frame="a", axis="equal", fmt="png", show=true)
+```
+
+!["Blue rectangle"](figures/rect_blue.png)
+
+## Rectangles with patterns
+
+Now add some patterns. The full pattern syntax is explained in [GMT patterns]
+(http://gmt.soest.hawaii.edu/doc/latest/GMT_Docs.html#specifying-area-fill-attributes)
+but basically we are using pattern number 20 at 200 dpi and a blue background for the left rectangle
+and pattern 89 also at 200 dpis for the right rectangle.
+
+```julia
+using GMT
+rect = [1 1; 1 7; 4 7; 4 1; 1 1];
+plot(rect, region=[0 10 0 10], lw=1, fill="p20+bgreen+r200", frame="a", axis="equal")
+plot!([4 0].+rect, lw=1, fill="p89+r200", fmt="png", show=true)
+```
+
+!["Pattern Rectangles"](figures/rect_patterns.png)
+
+## Rectangles with transparency
+
+This variation creates rectangles with 0, 30% and 70% transparency as well as different boundary lines.
+
+```julia
+using GMT
+rect = [0.5 0.5; 0.5 7; 2.5 7; 2.5 0.5; 0.5 0.5];
+plot(rect, region=[0 10 0 10], lw=0.5, fill="blue", frame="a", axis="equal")
+plot!([3 0].+rect, lw=1, ls="--", fill="blue", transparency=30)
+plot!([6 0].+rect, lw=2, lc="red", fill="blue", transparency=70, fmt="png", show=true)
+```
+
+!["Transparent Rectangles"](figures/rect_alpha.png)
--- src/GMT.jl
+++ src/GMT.jl
@@ -2,7 +2,7 @@ __precompile__()
 
 module GMT
 
-const GMTver = 5.0
+const GMTver = 6.0
 const FMT = "ps"
 
 export
@@ -29,23 +29,15 @@ export
 	GMT_Write_Data,
 	GMT_Init_VirtualFile, GMT_Open_VirtualFile, GMT_Close_VirtualFile, GMT_Read_VirtualFile,
 	GMT_Get_Matrix, GMT_Put_Matrix, GMT_Get_Vector, GMT_Put_Vector,
-	GMT_IS_DATASET, GMT_IS_TEXTSET, GMT_IS_GRID, GMT_IS_LINE,
-	GMT_IS_CPT, GMT_IS_IMAGE, GMT_IS_VECTOR, GMT_IS_MATRIX,
-	GMT_IS_POINT, GMT_IS_SURFACE, GMT_DATASET, GMT_IS_PLP,
-	GMT_GRID, GMT_MATRIX, GMT_PALETTE, GMT_PS,
-	GMT_UNIVECTOR, GMT_IN, GMT_OUT,
-	GMT_VIA_NONE, GMT_VIA_VECTOR, GMT_VIA_MATRIX, GMT_VIA_OUTPUT,
-	GMT_IS_DUPLICATE_VIA_VECTOR, GMT_IS_REFERENCE_VIA_VECTOR,
-	GMT_IS_DUPLICATE_VIA_MATRIX, GMT_IS_REFERENCE_VIA_MATRIX,
-	GMT_MODULE_EXIST, GMT_MODULE_PURPOSE, GMT_MODULE_OPT, GMT_MODULE_CMD,
-	GMT_GRID_ALL,
+	GMT_DATASET, GMT_GRID, GMT_MATRIX, GMT_PALETTE, GMT_UNIVECTOR,
+	GMT_VIA_OUTPUT,
 	GMT_Report, array_container,
 	GMT_Encode_Options, GMT_Expand_Option, gmtlib_grd_flip_vertical,
 	gmt_core_module_info,
 	gmt, grid_type,
 	GMT_RESOURCE, GMTAPI_CTRL, GMTAPI_DATA_OBJECT,
 	basemap, basemap!, coast, coast!, xy, xy!, grdcontour, grdcontour!, grdimage, grdimage!, grdtrack,
-	grdview, grdview!, makecpt, histogram, histogram!, image, image!, psconvert, scale, scale!,
+	grdview, grdview!, makecpt, histogram, histogram!, image, image!, psconvert, colorbar, colorbar!,
 	rose, rose!, solar, solar!, text, text!, gmtinfo, grdinfo, surface, triangulate,
 	nearneighbor, imshow, imshow!, plot, plot!, splitxyz, wiggle, wiggle!,
 	text_record,
