2869c850b38d394faf4bb9194d5a40912e4c817e
Joaquim Luis <jmfluis@gmail.com>
2018-05-14 01:00:20 +0100

    Export the block*s

--- src/GMT.jl
+++ src/GMT.jl
@@ -30,7 +30,7 @@ export
 	GMTgrid, GMTimage, GMTcpt, GMTdataset, GMTps,
 	GMT_Encode_Options, GMT_Expand_Option, gmtlib_grd_flip_vertical,
 	gmt, grid_type,
-	basemap, basemap!, coast, coast!, logo, logo!, xy, xy!, grdcontour, grdcontour!, grdimage,
+	basemap, basemap!, blockmean, blockmedian, blockmode, coast, coast!, logo, logo!, xy, xy!, grdcontour, grdcontour!, grdimage,
 	grdimage!, grdtrack, grdview, grdview!, makecpt, histogram, histogram!, image, image!, psconvert,
 	colorbar, colorbar!, rose, rose!, solar, solar!, text, text!, gmtinfo, grdinfo, surface,
 	triangulate, nearneighbor, imshow, imshow!, plot, plot!, plot3d, plot3d!, splitxyz, wiggle, wiggle!,
@@ -43,6 +43,7 @@ include("libgmt.jl")
 include("gmt_main.jl")
 include("common_options.jl")
 include("gmtinfo.jl")
+include("blocks.jl")
 include("gmtlogo.jl")
 include("grdcontour.jl")
 include("grdinfo.jl")
