98cdfa76cccab7c6aa9c6aa7c21e07f5af12fc1f
Joaquim Luis <jmfluis@gmail.com>
2018-08-13 02:18:36 +0100

    New module

---
+++ src/grdtrend.jl
@@ -0,0 +1,94 @@
+"""
+	grdtrend(cmd0::String="", arg1=[], arg2=[]; kwargs...)
+
+Reads a 2-D grid file or a GMTgrid type and produces a contour map by tracing each
+contour through the grid.
+
+Full option list at [`grdtrend`](http://gmt.soest.hawaii.edu/doc/latest/grdtrend.html)
+
+Parameters
+----------
+
+- **N** : **model** : -- Str or Number --
+	Contours to be drawn.
+	[`-N`](http://gmt.soest.hawaii.edu/doc/latest/grdtrend.html#n)
+- **D** : **diff** : -- Str or [] --
+	Dump contours as data line segments; no plotting takes place.
+	[`-D`](http://gmt.soest.hawaii.edu/doc/latest/grdtrend.html#d)
+- **T** : **trend** : -- Str or [] --
+	Output the trend surface 
+	[`-T`](http://gmt.soest.hawaii.edu/doc/latest/grdtrend.html#t)
+- **W** : **weights** : -- Str --
+	Used to 
+	[`-W`](http://gmt.soest.hawaii.edu/doc/latest/grdtrend.html#w)
+- $(GMT.opt_R)
+- $(GMT.opt_V)
+"""
+function grdtrend(cmd0::String="", arg1=[], arg2=[]; kwargs...)
+
+	length(kwargs) == 0 && return monolitic("grdtrend", cmd0, arg1, arg2)	# Speedy mode
+
+	if (isempty(cmd0) && isempty_(arg1))
+		error("Must provide the grid to work with.")
+	end
+
+	d = KW(kwargs)
+
+	cmd, opt_R = parse_R("", d)
+	cmd = parse_V(cmd, d)
+
+	cmd = add_opt(cmd, 'N', d, [:N :model])
+	cmd = add_opt(cmd, 'D', d, [:D :diff])
+	cmd = add_opt(cmd, 'T', d, [:T :trend])
+	cmd = add_opt(cmd, 'W', d, [:W :weights])
+
+	if (occursin(cmd, "-D") && occursin(cmd, "-T"))
+		warning("Usage error, both difference and trend were required. Ignoring the trend request.")
+	end
+
+	ff = findfirst("-D", cmd)
+	if (ff == nothing)
+		ff = findfirst("-T", cmd)
+	end
+	ind = (ff == nothing) ? 0 : first(ff)
+	if (ind > 0 && cmd[ind+2] != ' ')      # A file name was provided
+		no_output = true
+	else
+		no_output = false
+		if (ind == 0)						# No -T -or -D provided so default to -T
+			cmd = cmd * " -T"
+		end
+	end
+
+	if (isempty_(arg1) && !isempty(cmd0))	# Grid was passed as file name
+		cmd = cmd0 * " " * cmd
+		(haskey(d, :Vd)) && println(@sprintf("\tgrdtrend %s", cmd))
+		if (no_output)
+			if (!isempty_(arg2))  gmt("grdtrend " * cmd, arg2)
+			else                  gmt("grdtrend " * cmd)
+			end
+			return nothing
+		else
+			if (!isempty_(arg2))  O = gmt("grdtrend " * cmd, arg2)
+			else                  O = gmt("grdtrend " * cmd)
+			end
+			return O
+		end
+	else
+		(haskey(d, :Vd)) && println(@sprintf("\tgrdtrend %s", cmd))
+		if (no_output)
+			if (!isempty_(arg2))  gmt("grdtrend " * cmd, arg1, arg2)
+			else                  gmt("grdtrend " * cmd, arg1)
+			end
+			return nothing
+		else
+			if (!isempty_(arg2))  O = gmt("grdtrend " * cmd, arg1, arg2)
+			else                  O = gmt("grdtrend " * cmd, arg1)
+			end
+			return O
+		end
+	end
+end
+
+# ---------------------------------------------------------------------------------------------------
+grdtrend(arg1=[], arg2=[], cmd0::String=""; kw...) = grdtrend(cmd0, arg1, arg2; kw...)
