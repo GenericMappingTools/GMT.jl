c18d24abe4787d3cb36be48b6dfdae583361813b
Joaquim Luis <jmfluis@gmail.com>
2018-08-24 23:28:41 +0100

    Add grdlandmask & greenspline

---
+++ src/grdlandmask.jl
@@ -0,0 +1,66 @@
+"""
+	grdlandmask(cmd0::String="", arg1=[], kwargs...)
+
+Reads the selected shoreline database and uses that information to decide which nodes in the
+specified grid are over land or over water.
+
+Full option list at [`grdlandmask`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html)
+
+Parameters
+----------
+
+- $(GMT.opt_R)
+- **I** : **inc** : -- Str or Number --
+
+    *x_inc* [and optionally *y_inc*] is the grid spacing.
+    [`-I`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html#i)
+- **A** : **area** : -- Str or Number --
+
+    Features with an area smaller than min_area in km^2 or of
+    hierarchical level that is lower than min_level or higher than
+    max_level will not be plotted.
+    [`-A`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html#a)
+- **D** : **res** : **resolution** : -- Str --
+
+    Selects the resolution of the data set to use ((f)ull, (h)igh, (i)ntermediate, (l)ow, and (c)rude).
+    [`-D`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html#d)
+- **E** : **bordervalues** : -- Str or List --    Flags = cborder/lborder/iborder/pborder or bordervalue
+
+    Nodes that fall exactly on a polygon boundary should be considered to be outside the polygon
+    [Default considers them to be inside].
+    [`-E`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html#e)
+- **G** : **outgrid** : -- Str --
+
+    Output grid file name. Note that this is optional and to be used only when saving
+    the result directly on disk. Otherwise, just use the G = grdlandmask(....) form.
+    [`-G`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html#g)
+- **N** : **maskvalues** : -- Str or List --    Flags = wet/dry or ocean/land/lake/island/pond
+
+    Sets the values that will be assigned to nodes. Values can be any number, including the textstring NaN
+    [`-N`](http://gmt.soest.hawaii.edu/doc/latest/grdlandmask.html#n)
+- $(GMT.opt_V)
+- $(GMT.opt_r)
+- $(GMT.opt_x)
+"""
+function grdlandmask(cmd0::String=""; kwargs...)
+
+	length(kwargs) == 0 && (findfirst(" -", cmd0) != nothing) && return monolitic("grdlandmask", cmd0)	# Speedy mode
+
+	d = KW(kwargs)
+
+	cmd, opt_R = parse_R("", d)
+	cmd = parse_V(cmd, d)
+	cmd = parse_r(cmd, d)
+	cmd = parse_x(cmd, d)
+	cmd = parse_params(cmd, d)
+
+	cmd = add_opt(cmd, 'A', d, [:A :area])
+	cmd = add_opt_s(cmd, 'D', d, [:D :res :resolution])
+	cmd = add_opt(cmd, 'E', d, [:E :bordervalues])
+    cmd = add_opt(cmd, 'I', d, [:I :inc])
+	cmd = add_opt_s(cmd, 'G', d, [:G :outgrid])
+	cmd = add_opt(cmd, 'N', d, [:N :maskvalues])
+
+	no_output = common_grd(cmd, 'G')		# See if an output is requested (or write result in grid file)
+    return common_grd(d, " ", cmd, [], [], no_output, "grdlandmask")	# Shared by several grdxxx modules
+end
--- src/greenspline.jl
+++ src/greenspline.jl
@@ -1,11 +1,11 @@
 """
-    surface(cmd0::String="", arg1=[]; kwargs...)
+    greenspline(cmd0::String="", arg1=[]; kwargs...)
 
 Reads randomly-spaced (x,y,z) triples and produces a binary grid file of gridded values z(x,y) by solving:
 	
 		(1 - T) * L (L (z)) + T * L (z) = 0
 
-Full option list at [`surface`](http://gmt.soest.hawaii.edu/doc/latest/surface.html)
+Full option list at [`greenspline`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html)
 
 Parameters
 ----------
@@ -13,99 +13,88 @@ Parameters
 - $(GMT.opt_R)
 - **I** : **inc** : -- Str or Number --
 	*x_inc* [and optionally *y_inc*] is the grid spacing.
-    [`-I`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#i)
+    [`-I`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#i)
 - **A** : **aspect_ratio** : -- Number --
     Aspect ratio. If desired, grid anisotropy can be added to the equations.
-    [`-A`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#a)
+    [`-A`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#a)
 - **C** : **convergence** : -- Number --
 	Convergence limit. Iteration is assumed to have converged when the maximum absolute change in any
 	grid value is less than convergence_limit.
-    [`-C`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#c)
+    [`-C`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#c)
 - **G** : **grid** : -- Str or [] --
 	Optional output grid file name. If not provided return a GMTgrid type.
-    [`-G`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#g)
+    [`-G`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#g)
 - **Ll** : **lower** : -- Str or Number --
 	Impose limits on the output solution. lower sets the lower bound. lower can be the name of a grid
 	file with lower bound values, a fixed value, d to set to minimum input value,
-    [`-L`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#l)
+    [`-L`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#l)
 - **Lu** : **upper** : -- Str or Number --
-    [`-L`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#l)
+    [`-L`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#l)
 - **N** : **max_iterations** : -- Number --
 	Number of iterations. Iteration will cease when convergence_limit is reached or when number of
 	iterations reaches max_iterations.
-    [`-N`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#n)
+    [`-N`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#n)
 - **Q** : **suggest** : -- Bool or [] --
     Suggest grid dimensions which have a highly composite greatest common factor.
-    [`-Q`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#q)
+    [`-Q`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#q)
 - **S** : **search_radius** : -- Number or Str --  
     Sets the resolution of the projected grid that will be created.
-    [`-S`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#s)
+    [`-S`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#s)
 - **T** : **tension** : -- Number or Str --
     Tension factor[s]. These must be between 0 and 1.
-    [`-T`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#t)
+    [`-T`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#t)
 - $(GMT.opt_V)
 - **Z** : **over_relaxation** : -- Str or GMTgrid --
     Over-relaxation factor. This parameter is used to accelerate the convergence; it is a number between 1 and 2.
-    [`-Z`](http://gmt.soest.hawaii.edu/doc/latest/surface.html#z)
-- $(GMT.opt_a)
+    [`-Z`](http://gmt.soest.hawaii.edu/doc/latest/greenspline.html#z)
 - $(GMT.opt_bi)
-- $(GMT.opt_di)
+- $(GMT.opt_d)
 - $(GMT.opt_e)
 - $(GMT.opt_f)
 - $(GMT.opt_h)
 - $(GMT.opt_i)
-- $(GMT.opt_r)
+- $(GMT.opt_o)
+- $(GMT.opt_x)
 - $(GMT.opt_swap_xy)
 """
-function surface(cmd0::String="", arg1=[]; data=[], kwargs...)
+function greenspline(cmd0::String="", arg1=[]; data=[], kwargs...)
 
-	length(kwargs) == 0 && isempty(data) && return monolitic("surface", cmd0, arg1)	# Speedy mode
+	length(kwargs) == 0 && isempty(data) && return monolitic("greenspline", cmd0, arg1)	# Speedy mode
 
 	d = KW(kwargs)
 	cmd = ""
 	cmd, opt_R = parse_R(cmd, d)
 	cmd = parse_V(cmd, d)
-	cmd = parse_a(cmd, d)
 	cmd, = parse_bi(cmd, d)
-	cmd, = parse_di(cmd, d)
+	cmd, = parse_d(cmd, d)
 	cmd = parse_e(cmd, d)
 	cmd = parse_f(cmd, d)
 	cmd = parse_h(cmd, d)
 	cmd, = parse_i(cmd, d)
-	cmd = parse_r(cmd, d)
+	cmd = parse_o(cmd, d)
+	cmd = parse_x(cmd, d)
 	cmd = parse_swap_xy(cmd, d)
+	cmd = parse_params(cmd, d)
 
-	cmd = add_opt(cmd, 'A', d, [:A :aspect_ratio])
-	cmd = add_opt(cmd, 'C', d, [:C :convergence])
+	cmd = add_opt(cmd, 'A', d, [:A :gradfile])
+	cmd = add_opt(cmd, 'C', d, [:C :approximate])
+	cmd = add_opt(cmd, 'D', d, [:D :mode])
+	cmd = add_opt(cmd, 'E', d, [:E :misfit])
 	cmd = add_opt(cmd, 'G', d, [:G :grid])
 	cmd = add_opt(cmd, 'I', d, [:I :inc])
-	cmd = add_opt(cmd, "Ll", d, [:Ll :lower])
-	cmd = add_opt(cmd, "Lu", d, [:Ll :upper])
-	cmd = add_opt(cmd, 'N', d, [:N :max_iterations])
-	cmd = add_opt(cmd, 'Q', d, [:Q :suggest])
-	cmd = add_opt(cmd, 'S', d, [:S :search_radius])
-	cmd = add_opt(cmd, 'T', d, [:T :tension])
-	cmd = add_opt(cmd, 'Z', d, [:Z :over_relaxation])
+	cmd = add_opt(cmd, "L", d, [:L :leave_trend])
+	cmd = add_opt(cmd, 'N', d, [:N :nodefile])
+	cmd = add_opt(cmd, 'Q', d, [:Q :direction_derivative])
+	cmd = add_opt(cmd, 'S', d, [:S :splines])
+	cmd = add_opt(cmd, 'T', d, [:T :maskgrid])
+	cmd = add_opt(cmd, 'W', d, [:W :uncertainties])
 
 	no_output = common_grd(cmd, 'G')		# See if an output is requested (or write result in grid file)
     cmd, arg1, = read_data(data, cmd, arg1)
 
-	(haskey(d, :Vd)) && println(@sprintf("\tsurface %s", cmd))
-
-	G = nothing
-	no_output = no_output || occursin("-Q", cmd)
-	if (no_output)
-		if (!isempty_(arg1))  gmt("surface " * cmd, arg1)
-		else                  gmt("surface " * cmd)
-		end
-	else
-		if (!isempty_(arg1))  G = gmt("surface " * cmd, arg1)
-		else                  G = gmt("surface " * cmd)
-		end
-	end
-	return G
+	return common_grd(d, cmd0, cmd, arg1, [], no_output, "greenspline")	# Shared by several grdxxx modules
 end
 
 # ---------------------------------------------------------------------------------------------------
-surface(arg1::GMTgrid, cmd0::String=""; data=[], kw...) = surface(cmd0, arg1; data=data, kw...)
-surface(arg1::Array, cmd0::String=""; data=[], kw...) = surface(cmd0, arg1; data=data, kw...)
+greenspline(arg1::GMTgrid, cmd0::String=""; data=[], kw...) = greenspline(cmd0, arg1; data=data, kw...)
+greenspline(arg1::Array, cmd0::String=""; data=[], kw...) = greenspline(cmd0, arg1; data=data, kw...)
