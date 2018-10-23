0201aff481a324d2594ae902a09fb305338f5fa5
Joaquim Luis <jmfluis@gmail.com>
2018-10-23 01:04:51 +0100

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using GMT
 using Test
 using LinearAlgebra
 
-#=
 try
 	run(`gmt --version`)	# Will fail if GMT is not installed.
 	global got_it = true
@@ -12,7 +11,6 @@ catch
 end
 
 if (got_it)					# Otherwise go straight to end
-=#
 
 	# write your own tests here
 	r = gmt("gmtinfo -C", ones(Float32,9,3)*5);
@@ -265,9 +263,9 @@ if (got_it)					# Otherwise go straight to end
 	d = Dict(:inc => "2");
 	r = GMT.parse_inc("",d,[:I :inc], "I");		@test r == " -I2"
 
-	r = vector_attrib(head_size=2.2,stop=[],norm=0.635,shape=:arrow,half_arrow=:right,
+	r = vector_attrib(head_size=2.2,stop=[],norm="0.25i",shape=:arrow,half_arrow=:right,
 	                  justify=:end,head_fill=:none,trim=0.1,xy=true,scale=6.6)
-	@test r == "2.2+e+je+r+g-+n0.635+h1+t0.1+s+z6.6"
+	@test r == "2.2+e+je+r+g-+n0.25i+h1+t0.1+s+z6.6"
 
 	# EXAMPLES
 	plot(collect(1:10),rand(10), lw=1, lc="blue", marker="square",
@@ -295,4 +293,4 @@ if (got_it)					# Otherwise go straight to end
 		rm("lixo.tif")
 	end
 
-#end					# End valid testing zone
+end					# End valid testing zone
