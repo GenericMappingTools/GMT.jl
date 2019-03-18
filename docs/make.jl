faf08df302e13f7199bf9f07d0005eceb15617a0
Joaquim Luis <jmfluis@gmail.com>
2019-03-18 02:43:30 +0000

    Try

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,8 @@ using Documenter, GMT
 
 makedocs(
     modules = [GMT],
-    #format = :html,
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    format = :html,
+    #format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     sitename = "GMT",
     assets = ["assets/custom.css"],
     pages = Any[
