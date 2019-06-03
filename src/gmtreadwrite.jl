7d80572b117ea04e21d406a199a0ab442e068986
Joaquim Luis <jmfluis@gmail.com>
2019-06-03 17:44:15 +0100

    Remove test for memlayout in gmtreadwrite

--- src/gmtreadwrite.jl
+++ src/gmtreadwrite.jl
@@ -213,9 +213,6 @@ function gmtwrite(fname::String, data; kwargs...)
 	if (opt_T == " -Ti" || opt_T == " -Tg")		# See if we have a mem layout request
 		if (haskey(d, :layout))
 			t = arg2str(d[:layout])
-			if (length(t) != 3)
-				error(@sprintf("Memory layout option must have 3 characters and not %s", t))
-			end
 			cmd = (opt_T == " -Ti") ? cmd * " -%" * t : cmd * " -&" * t
 		end
 	end
