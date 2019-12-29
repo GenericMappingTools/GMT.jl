7fc06180e6ed58c17fd4fdf7d4e5ac837d777626
Joaquim Luis <jmfluis@gmail.com>
2019-12-29 01:44:34 +0000

    Remove % from env vars. That is now automatic

--- test/main_sc.jl
+++ test/main_sc.jl
@@ -1,4 +1,4 @@
 # main-sc for movies
 gmtset(FONT_TAG="14p,Helvetica-Bold")
-grdimage("@USEast_Coast.nc", proj="G%MOVIE_COL0%/%MOVIE_COL1%/160/210/55/0/36/34/%MOVIE_WIDTH%+", region=:g, I="int_US.nc", C="globe_US.cpt", X=0, Y=0)
+grdimage("@USEast_Coast.nc", proj="GMOVIE_COL0/MOVIE_COL1/160/210/55/0/36/34/MOVIE_WIDTH+", region=:g, I="int_US.nc", C="globe_US.cpt", X=0, Y=0)
 plot("flight_path.txt", lw=1)
