72472c25a9565d29a50d93834310d57e11b794a9
Joaquim Luis <jmfluis@gmail.com>
2020-07-05 13:46:38 +0100

    Add option new in GMT6.1

--- src/sample1d.jl
+++ src/sample1d.jl
@@ -12,7 +12,7 @@ Parameters
 
     For track resampling (if -T…unit is set) we can select how this is to be performed.
     ($(GMTdoc)sample1d.html#a)
-- **F** | **interp_type** :: [Type => Str]   ``Arg = l|a|c|n[+1|+2]``
+- **F** | **interp_type** :: [Type => Str]   ``Arg = l|a|c|n|s<p>[+1|+2]``
 
     Choose from l (Linear), a (Akima spline), c (natural cubic spline), and n (no interpolation:
     nearest point) [Default is Akima].
@@ -22,11 +22,15 @@ Parameters
     Indicates which column contains the independent variable (time). The left-most column
     is # 0, the right-most is # (n_cols - 1). [Default is 0].
     ($(GMTdoc)sample1d.html#n)
-- **T** | **inc** | **equi_space** :: [Type => List | Str]     ``Arg = [min/max/]inc[+a|n]] or file|list``
+- **T** | **inc** | **range** :: [Type => List | Str]     ``Arg = [min/max/]inc[+a|n]] or file|list``
 
     Evaluate the best-fit regression model at the equidistant points implied by the arguments.
     ($(GMTdoc)sample1d.html#t)
 - $(GMT.opt_V)
+- **W** | **weights_col** :: [Type => Int]     ``Arg = w_col``
+
+    Sets the column number of the weights to be used with a smoothing cubic spline. Requires Fs. (GMT6.1)
+    ($(GMTdoc)sample1d.html#w)
 - $(GMT.opt_write)
 - $(GMT.opt_append)
 - $(GMT.opt_b)
@@ -45,7 +49,7 @@ function sample1d(cmd0::String="", arg1=nothing; kwargs...)
 
 	d = KW(kwargs)
 	cmd = parse_common_opts(d, "", [:V_params :b :d :e :f :g :h :i :o :yx])
-	cmd = parse_these_opts(cmd, d, [[:A :resamp], [:F :interp_type], [:N :time_col], [:T :inc :equi_space]])
+	cmd = parse_these_opts(cmd, d, [[:A :resamp], [:F :interp_type], [:N :time_col], [:T :inc :range], [:W :weights_col]])
 
 	common_grd(d, cmd0, cmd, "sample1d ", arg1)		# Finish build cmd and run it
 end
