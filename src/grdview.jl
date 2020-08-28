6647215f146586db360fa20185bd57ac8d840d36
Joaquim Luis <jmfluis@gmail.com>
2020-08-28 01:35:25 +0100

    More tests

--- src/grdview.jl
+++ src/grdview.jl
@@ -26,7 +26,7 @@ Full option list at [`grdview`]($(GMTdoc)grdview.html)
     Draws a plane at this z-level.
     ($(GMTdoc)grdview.html#n)
 - $(GMT.opt_P)
-- **Q** | **surftype** | **surf** :: [Type => Str | Int] ``Arg = mesh=Bool, surface=Boll, image=Bool, wterfall=(:rows|cols,[fill])``
+- **Q** | **surftype** | **surf** :: [Type => Str | Int] ``Arg = mesh=Bool, surface=Bool, image=Bool, wterfall=(:rows|cols,[fill])``
 
     Specify **m** for mesh plot, **s** for surface, **i** for image.
     ($(GMTdoc)grdview.html#q)
