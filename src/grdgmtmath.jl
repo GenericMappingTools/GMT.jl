45519fd010032db12589e1b511434697ebd2001b
Joaquim Luis <jmfluis@gmail.com>
2020-09-08 19:40:24 +0100

    Add args... argument

--- src/grdgmtmath.jl
+++ src/grdgmtmath.jl
@@ -1,5 +1,5 @@
 """
-    grdmath(cmd::String)
+    grdmath(cmd::String, args...)
 
 Call grdmath with all commands in a single string 'cmd'.
 This is not useful in itself as compared to call gmt("grdmath ....") but it's very useful
@@ -7,16 +7,16 @@ in 'movie' because it can generate shell scripts from the julai command
 
 Full option list at [`grdmath`]($(GMTdoc)grdmath.html)
 """
-function grdmath(cmd::String)
+function grdmath(cmd::String, args...)
 
 	d = KW()
 	cmd = "grdmath " * cmd
 	if (dbg_print_cmd(d, cmd) !== nothing)  return cmd  end		# The real useful bit od this fun
-	gmt(cmd)
+	gmt(cmd, args...)
 end
 
 """
-    gmtmath(cmd::String)
+    gmtmath(cmd::String, args...)
 
 Call gmtmath with all commands in a single string 'cmd'.
 This is not useful in itself as compared to call gmt("gmtmath ....") but it's very useful
@@ -24,10 +24,10 @@ in 'movie' because it can generate shell scripts from the julai command
 
 Full option list at [`grdmath`]($(GMTdoc)grdmath.html)
 """
-function gmtmath(cmd::String)
+function gmtmath(cmd::String, args...)
 
 	d = KW()
 	cmd = "gmtmath " * cmd
 	if (dbg_print_cmd(d, cmd) !== nothing)  return cmd  end
-	gmt(cmd)
+	gmt(cmd, args...)
 end
