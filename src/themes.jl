0cabb8da926a04107d5f3defe2129676e04c603f
Joaquim Luis <jmfluis@gmail.com>
2020-12-05 17:11:55 +0000

    Fix var access bug

--- src/themes.jl
+++ src/themes.jl
@@ -13,22 +13,23 @@ function theme(name; kwargs...)
 		end
 		(bg_color == "") && (bg_color = "5/5/35")
 
-		gmtlib_setparameter(API, "FONT_ANNOT_PRIMARY", "12p,$(fonts[1]),$(color[1])")
-		gmtlib_setparameter(API, "FONT_ANNOT_SECONDARY", "14p,$(fonts[1]),$(color[])")
-		gmtlib_setparameter(API, "FONT_HEADING", "32p,$(fonts[2]),$(color[1])")
+		gmtlib_setparameter(API, "FONT_ANNOT_PRIMARY", "12p,$(fonts[1]),$(colors[1])")
+		gmtlib_setparameter(API, "FONT_ANNOT_SECONDARY", "14p,$(fonts[1]),$(colors[1])")
+		gmtlib_setparameter(API, "FONT_HEADING", "32p,$(fonts[2]),$(colors[1])")
 		gmtlib_setparameter(API, "FONT_LABEL", "16p,$(fonts[1]),black")
-		gmtlib_setparameter(API, "FONT_LOGO", "8p,$(fonts[3]),$(color[1])")
-		gmtlib_setparameter(API, "FONT_TAG", "20p,$(fonts[1]),$(color[1])")
-		gmtlib_setparameter(API, "FONT_TITLE", "24p,$(fonts[2]),$(color[1])")
+		gmtlib_setparameter(API, "FONT_LOGO", "8p,$(fonts[3]),$(colors[1])")
+		gmtlib_setparameter(API, "FONT_TAG", "20p,$(fonts[1]),$(colors[1])")
+		gmtlib_setparameter(API, "FONT_TITLE", "24p,$(fonts[2]),$(colors[1])")
 
-		gmtlib_setparameter(API, "MAP_DEFAULT_PEN", "default,$(color[1])")
-		gmtlib_setparameter(API, "MAP_FRAME_PEN", "thicker,$(color[1])")
-		gmtlib_setparameter(API, "MAP_GRID_PEN_PRIMARY", "default,$(color[1])")
-		gmtlib_setparameter(API, "MAP_GRID_PEN_SECONDARY", "thinner,$(color[2])")
-		gmtlib_setparameter(API, "MAP_TICK_PEN_PRIMARY", "thinner,$(color[1])")
-		gmtlib_setparameter(API, "MAP_TICK_PEN_SECONDARY", "thinner,$(color[2])")
+		gmtlib_setparameter(API, "MAP_DEFAULT_PEN", "default,$(colors[1])")
+		gmtlib_setparameter(API, "MAP_FRAME_PEN", "thicker,$(colors[1])")
+		gmtlib_setparameter(API, "MAP_GRID_PEN_PRIMARY", "default,$(colors[1])")
+		gmtlib_setparameter(API, "MAP_GRID_PEN_SECONDARY", "thinner,$(colors[2])")
+		gmtlib_setparameter(API, "MAP_TICK_PEN_PRIMARY", "thinner,$(colors[1])")
+		gmtlib_setparameter(API, "MAP_TICK_PEN_SECONDARY", "thinner,$(colors[2])")
 		gmtlib_setparameter(API, "PS_PAGE_COLOR", "$bg_color")
 		ThemeIsOn[1] = true
+#=
 	elseif (name == :modern || name == "modern")
 		if (font == "")  fonts = ["AvantGarde-Book", "AvantGarde-Demi", "Helvetica"]	# The GMT settings
 		else             fonts = [font, font, font]
@@ -36,13 +37,13 @@ function theme(name; kwargs...)
 		(color == "") && (color = "black")
 		(bg_color == "") && (bg_color = "white")
 
-		gmtlib_setparameter(API, "FONT_ANNOT_PRIMARY", "auto,$(font[1]),$color")
-		gmtlib_setparameter(API, "FONT_ANNOT_SECONDARY", "14p,$(font[1]),$color")
-		gmtlib_setparameter(API, "FONT_HEADING", "auto,$(font[2]),$color")
-		gmtlib_setparameter(API, "FONT_LABEL", "auto,$(font[1]),$color")
-		gmtlib_setparameter(API, "FONT_LOGO", "auto,$(font[3]),$color")
-		gmtlib_setparameter(API, "FONT_TAG", "auto,$(font[1]),$color")
-		gmtlib_setparameter(API, "FONT_TITLE", "auto,$(font[2]),$color")
+		gmtlib_setparameter(API, "FONT_ANNOT_PRIMARY", "auto,$(fonts[1]),$color")
+		gmtlib_setparameter(API, "FONT_ANNOT_SECONDARY", "14p,$(fonts[1]),$color")
+		gmtlib_setparameter(API, "FONT_HEADING", "auto,$(fonts[2]),$color")
+		gmtlib_setparameter(API, "FONT_LABEL", "auto,$(fonts[1]),$color")
+		gmtlib_setparameter(API, "FONT_LOGO", "auto,$(fonts[3]),$color")
+		gmtlib_setparameter(API, "FONT_TAG", "auto,$(fonts[1]),$color")
+		gmtlib_setparameter(API, "FONT_TITLE", "auto,$(fonts[2]),$color")
 		gmtlib_setparameter(API, "FORMAT_GEO_MAP", "ddd:mm:ssF")
 		gmtlib_setparameter(API, "MAP_FRAME_AXES", "WrStZ")
 		gmtlib_setparameter(API, "MAP_ANNOT_MIN_SPACING", "auto")
@@ -58,6 +59,7 @@ function theme(name; kwargs...)
 		gmtlib_setparameter(API, "MAP_VECTOR_SHAPE", "auto")
 		gmtlib_setparameter(API, "PS_PAGE_COLOR", "$bg_color")
 		ThemeIsOn[1] = true
+=#
 	else
 		ThemeIsOn[1] = false			# Because we called reset_defaults() above
 	end
