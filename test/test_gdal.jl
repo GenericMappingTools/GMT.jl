bf115d8e99e532aa6402b525611e2118986983f4
Joaquim Luis <jmfluis@gmail.com>
2021-02-27 14:18:18 +0000

    Try again gdaldem

--- test/test_gdal.jl
+++ test/test_gdal.jl
@@ -63,12 +63,13 @@ Gdal.GDALDestroyDriverManager()
 	ds_small = readgd("utmsmall.tif");
 	gdalinfo(ds_small, [""]);
 	gdalwarp(ds_small, [""]);
-	#gdaldem(ds_small, "hillshade", [""]);
+	gdaldem(ds_small, "hillshade", ["-q"]);
 	gdaltranslate(ds_small, [""]);
 
 	ds_point = readgd("point.geojson");
 	ds_grid = gdalgrid(ds_point, ["-of","MEM","-outsize","3", "10","-txe","100","100.3","-tye","0","0.1"]);
 	@test getgeotransform(ds_grid) ≈ [100.0,0.1,0.0,0.0,0.0,0.01]
+	readgd(ds_grid)
 
 	ds_csv = gdalvectortranslate(ds_point, ["-f","CSV","-lco", "GEOMETRY=AS_XY"], dest = "point.csv");
 	#=
