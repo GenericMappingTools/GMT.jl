2e89646505f8701d2cb053415a45ee668acb801a
Joaquim Luis <jmfluis@gmail.com>
2021-03-02 22:46:41 +0000

    Fix regressions in Modern mode and a stupid typo in current_cpt (lost last char)

--- src/psxy.jl
+++ src/psxy.jl
@@ -89,7 +89,7 @@ function common_plot_xyz(cmd0::String, arg1, caller::String, first::Bool, is3D::
 
 	# Look for color request. Do it after error bars because they may add a column
 	len = length(cmd);	n_prev = N_args;
-	cmd, args, n, got_Zvars = add_opt(d, cmd, 'Z', [:Z :zcolor :level], :data, Any[arg1, arg2, arg3], (outline="_+l", fill="_+f"))
+	cmd, args, n, got_Zvars = add_opt(d, cmd, 'Z', [:Z :level], :data, Any[arg1, arg2, arg3], (outline="_+l", fill="_+f"))
 	if (n > 0)
 		arg1, arg2, arg3 = args[:]
 		N_args = n
