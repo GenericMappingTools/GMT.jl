164137faf25712ae751c4c94223706de2b70178a
Joaquim Luis <jmfluis@gmail.com>
2021-04-06 16:01:37 +0100

    Let gdaldem hillshade parse kwarg arguments

--- src/gdal_tools.jl
+++ src/gdal_tools.jl
@@ -6,9 +6,8 @@ Convert raster data between different formats and other operations also provided
 The kwargs options accept the GMT region (-R), increment (-I), target SRS (-J) any of the keywords
 'outgrid', 'outfile' or 'save' = outputname options to make this function save the result in disk
 in the file 'outputname'. The file format is picked from the 'outputname' file extension.
-When any of the GMT options is used and no output file name is provided it returns a GMT object
-(either a grid or an image, depending on the input type). To force the return of a GDAL dataset
-use the option 'gdataset=true'
+When no output file name is provided it returns a GMT object (either a grid or an image, depending
+on the input type). To force the return of a GDAL dataset use the option 'gdataset=true'
 
   - INDATA - Input data. It can be a file name, a GMTgrid or GMTimage object or a GDAL dataset
   - OPTS   - List of options. The accepted options are the ones of the gdal_translate utility.
@@ -41,7 +40,7 @@ end
 
 # ---------------------------------------------------------------------------------------------------
 """
-    gdaldem(dataset::Dataset, method::String, options=String[]; dest="/vsimem/tmp", colorfile)
+    gdaldem(dataset::Dataset, method::String, options=String[]; dest="/vsimem/tmp", colorfile, kw...)
 
 Tools to analyze and visualize DEMs.
 
@@ -49,16 +48,40 @@ Tools to analyze and visualize DEMs.
 * **dataset**: The source dataset.
 * **method**: the processing to apply (one of "hillshade", "slope",
     "aspect", "color-relief", "TRI", "TPI", "Roughness").
-* **options**: List of options (potentially including filename and open
-    options). The accepted options are the ones of the gdaldem utility.
+* **options**: List of options (potentially including filename and open options).
+    The accepted options are the ones of the gdaldem utility.
 
 # Keyword Arguments
 * **colorfile**: color file (mandatory for "color-relief" processing, should be empty otherwise).
+* **kw...**: keyword=value arguments when `method` is hillshade.
 
 ### Returns
 A GMT grid or Image, or a GDAL dataset (or nothing if file was writen on disk).
 """
 function gdaldem(indata, method::String, opts=String[]; dest="/vsimem/tmp", kwargs...)
+	if (method == "hillshade")		# So far the only method that accept kwarg options
+		d = GMT.KW(kwargs)
+		band = ((val = GMT.find_in_dict(d, [:band])[1]) !== nothing) ? string(val) : "1"
+		opts = ["-compute_edges", "-b", band]
+		if ((val = GMT.find_in_dict(d, [:scale])[1]) === nothing)
+			if (isa(indata, GMT.GMTgrid) && (occursin("longlat", indata.proj4) || occursin("latlong", indata.proj4)) ||
+											grdinfo(indata, C="n")[1].data[end] == 1)
+				append!(opts, ["-s", "111120"])
+			end
+		else
+			append!(opts, ["-s", string(val)])
+		end
+		((val = GMT.find_in_dict(d, [:zfactor :zFactor])[1]) !== nothing) && append!(opts, ["-z", string(val)])
+		((val = GMT.find_in_dict(d, [:azim :azimuth])[1]) !== nothing) && append!(opts, ["-az", string(val)])
+		((val = GMT.find_in_dict(d, [:elev :altitude])[1]) !== nothing) && append!(opts, ["-alt", string(val)])
+		((val = GMT.find_in_dict(d, [:combined :combine])[1]) !== nothing) && append!(opts, ["-combined"])
+		((val = GMT.find_in_dict(d, [:multi :multidir :multiDirectional])[1]) !== nothing) && append!(opts, ["-multidirectional"])
+		((val = GMT.find_in_dict(d, [:igor])[1]) !== nothing) && append!(opts, ["-igor"])
+		((val = GMT.find_in_dict(d, [:alg])[1]) !== nothing) && append!(opts, ["-alg", string(val)])
+		((val = GMT.find_in_dict(d, [:Horn])[1]) !== nothing) && append!(opts, ["-alg", "Horn"])
+		((val = GMT.find_in_dict(d, [:Zeven :Zevenbergen])[1]) !== nothing) && append!(opts, ["-alg", "ZevenbergenThorne"])
+		((val = GMT.find_in_dict(d, [:Vd])[1]) !== nothing) && println(opts)
+	end
 	helper_run_GDAL_fun(gdaldem, indata, dest, opts, method, kwargs...)
 end
 
@@ -71,9 +94,8 @@ function helper_run_GDAL_fun(f::Function, indata, dest::String, opts::Vector{Str
 	# For gdaldem color-relief we need a further arg that is the name of a cpt. So save one on disk
 	_cmap = C_NULL
 	if (f == gdaldem && ((cmap = GMT.find_in_dict(d, [:C :color :cmap])[1])) !== nothing)
-		if (!isa(_cmap, String))
-			_cmap = tempdir() * "/GMTtmp_cpt"
-			GMT.gmtwrite(_cmap, cmap)
+		if ((isa(cmap, String) && (lowercase(splitext(cmap)[2][2:end]) == "cpt")) || isa(cmap, GMT.GMTcpt))
+			save_cpt4gdal(cmap, tempdir() * "/GMTtmp_cpt.cpt")	# GDAL pretend to recognise CPTs but it almost doesn't
 		else
 			_cmap = cmap
 		end
@@ -84,13 +106,29 @@ function helper_run_GDAL_fun(f::Function, indata, dest::String, opts::Vector{Str
 	CPLPushErrorHandler(@cfunction(CPLQuietErrorHandler, Cvoid, (UInt32, Cint, Cstring)))
 	((outname = GMT.add_opt(d, "", "", [:outgrid :outfile :save])) != "") && (dest = outname)
 	o = (method == "") ? f(dataset, opts; dest=dest) : f(dataset, method, opts; dest=dest, colorfile=_cmap)
+	(o.ptr == C_NULL) && @warn("$(f) returned a NULL pointer.")
 	if (o !== nothing)
-		# If any GMT opt is used and not explicitly stated to return a GDAL datase, return a GMT type
+		# If not explicitly stated to return a GDAL datase, return a GMT type
 		n_bands = (got_GMT_opts && !haskey(d, :gdataset) && isa(o, AbstractRasterBand)) ? 1 : nraster(o)
-		(got_GMT_opts && !haskey(d, :gdataset)) && (o = gd2gmt(o, bands=collect(1:n_bands)))
+		(!haskey(d, :gdataset)) && (o = gd2gmt(o, bands=collect(1:n_bands)))
 	end
 	CPLPopErrorHandler();
 	o
+end
+
+# Because the GDAL reconnaissance  of GMT cpts is very very weak, we must re-write CPTs in a way that it can swallow
+save_cpt4gdal(cpt::String, outname::String) = save_cpt4gdal(GMT.gmtread(cpt), outname)
+function save_cpt4gdal(cpt::GMT.GMTcpt, outname::String)
+	fid = open(outname, "w")
+	try
+		println(fid, "#COLOR_MODEL = RGB\n#")
+		for k = 1:size(cpt.range,1)
+			@printf(fid, "%.12g\t%.0f %.0f %.0f\t%.12g\t%.0f %.0f %.0f\n", cpt.range[k,1], cpt.cpt[k,1]*255, cpt.cpt[k,2]*255, cpt.cpt[k,3]*255, cpt.range[k,2], cpt.cpt[k,4]*255, cpt.cpt[k,5]*255, cpt.cpt[k,6]*255)
+		end
+	catch err
+		@warn("Error $(err) saving a GDAL pleasing CPT file $(outname)")
+	end
+	close(fid)
 end
 
 # ---------------------------------------------------------------------------------------------------
