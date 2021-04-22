ec9cbc2874d6fe3dab99ba9b211effdd64ea2349
Joaquim Luis <jmfluis@gmail.com>
2021-04-22 04:09:21 +0100

    Add more methods to setproj!. Expand it to GMT types.

--- src/gdal.jl
+++ src/gdal.jl
@@ -2278,6 +2278,34 @@ end
 
 	include("gdal_tools.jl")
 
+	# ---------------------------------------------------------------------------------------------------
+	"""
+		setproj!(type, proj)
+
+	Set a referencing system to the `type` object. This object can be a `GMTgrid`, a `GMTimage` or a
+	`GMTdataset`.
+
+	- `proj` Is either a Proj4 string or a WKT. Alternatively, it can also be another grid, image or dataset
+			type, in which case its referencing system is copied into `type`
+	"""
+	function setproj!(tipo::AbstractArray, proj::String="")
+		(!isa(tipo, GMT.GMTgrid) && !isa(tipo, GMT.GMTimage) && !isa(tipo, GMT.GMTdataset) && !isa(tipo, Vector{GMT.GMTdataset})) &&
+			error("Wrong data type for this function. Must be a grid, image or dataset")
+		(proj == "") && error("the projection string cannot obviously be empty")
+		isproj4 = (startswith(proj, "+proj") !== nothing)
+		obj = (isa(tipo, Vector{GMT.GMTdataset})) ? tipo[1] : tipo
+		(isproj4) ? (obj.proj4 = proj) : (obj.wkt = proj)
+		return nothing
+	end
+	function setproj!(tipo::AbstractArrayi, ref)
+		(!isa(ref, GMT.GMTgrid) && !isa(ref, GMT.GMTimage) && !isa(ref, GMT.GMTdataset) && !isa(ref, Vector{GMT.GMTdataset})) &&
+			error("Wrong REFERENCE data type for this function. Must be a grid, image or dataset")
+		obj = (isa(ref, Vector{GMT.GMTdataset})) ? ref[1] : ref
+		((prj = obj.proj4) == "") && (prj = obj.wkt)
+		(prj == "") && error("The REFERENCE type is not referenced with either PROJ4 or WKT string")
+		setproj!(tipo, prj)
+	end
+
 	# ------------ Aliases ------------
 	const creategd = create
 	const ogr2ogr  = gdalvectortranslate
