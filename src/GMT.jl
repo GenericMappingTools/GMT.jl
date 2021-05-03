23d4a79aeb7fbeceef51dfd9347de369e915c940
Joaquim Luis <jmfluis@gmail.com>
2021-05-03 03:47:58 +0100

    Export the lonlat2xy and xy2lonlat functions

--- src/GMT.jl
+++ src/GMT.jl
@@ -74,8 +74,8 @@ export
 	decorated, vector_attrib, wiggle, wiggle!, xyz2grd, gmtbegin, gmtend, gmthelp, subplot, gmtfig, inset, showfig,
 	earthtide, gmtgravmag3d, pscoupe, pscoupe!, coupe, coupe!, psmeca, psmeca!, meca, meca!, psvelo, psvelo!, velo, velo!,
 	mbimport, mbgetdata, mbsvplist, mblevitus,
-	blendimg!, mat2ds, mat2grid, mat2img, linspace, logspace, contains, fields, tic, toc, geodetic2enu, cpt4dcw,
-	gd2gmt, gmt2gd, gdalread, gdalshade, gdalwrite, varspacegrid, MODIS_L2, 
+	blendimg!, lonlat2xy, xy2lonlat, mat2ds, mat2grid, mat2img, linspace, logspace, contains, fields, tic, toc, geodetic2enu,
+	cpt4dcw, gd2gmt, gmt2gd, gdalread, gdalshade, gdalwrite, varspacegrid, MODIS_L2, 
 
 	getband, getdriver, getlayer, getproj, getgeom, getgeotransform, toPROJ4, toWKT, importPROJ4,
 	importWKT, importEPSG, gdalinfo, gdalwarp, gdaldem, gdaltranslate, gdalgrid, gdalvectortranslate, ogr2ogr,
