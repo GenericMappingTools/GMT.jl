5cf30ec3f714cf3497d2299c8c3b52180b9d6791
Joaquim Luis <jmfluis@gmail.com>
2021-11-10 18:08:13 +0000

    Let blockmean -S also return grids. Several improvements and option args expansions.

--- src/blocks.jl
+++ src/blocks.jl
@@ -11,17 +11,17 @@ Parameters
 - $(GMT.opt_R)
 - $(GMT.opt_I)
     ($(GMTdoc)blockmean.html#i)
-- **A** | **fields** :: [Type => Str]
+- **A** | **field** | **fields** :: [Type => Str]
 
     Select which fields to write to individual grids. Append comma-separated codes for available
-    fields: **z** (the mean data z, but see **-S**), **s** (standard deviation), **l** (lowest value),
-    **h** (highest value) and **w** (the output weight; requires **-W**). [Default is just **z**].
+    fields: **z** (the mean data z, but see **statistic**), **s** (standard deviation), **l** (lowest value),
+    **h** (highest value) and **w** (the output weight; requires **weights**). [Default is just **z**].
     ($(GMTdoc)blockmean.html#a)
 - **C** | **center** :: [Type => Bool]
 
     Use the center of the block as the output location [Default uses the mean location]. Not used when **-A**
     ($(GMTdoc)blockmean.html#c)
-- **E** | **extend** :: [Type => Str | []]
+- **E** | **extend** | **extended** :: [Type => Str | []]
 
     Provide Extended report which includes s (the standard deviation about the mean), l, the lowest
     value, and h, the high value for each block. Output order becomes x,y,z,s,l,h[,w]. [Default
@@ -39,6 +39,9 @@ Parameters
     Use `statistic=:n` to report the number of points inside each block, `statistic=:s` to report the sum of all z-values 
     inside a block, `statistic=:w` to report the sum of weights [Default (or `statistic=:m` reports mean value].
     ($(GMTdoc)blockmean.html#s)
+- **mean** :: [Type => Any] 
+
+    Report the mean value of points inside each block
 - **npts** | **counts** :: [Type => Any] 
 
     Report the number of points inside each block
@@ -48,6 +51,9 @@ Parameters
 - **sum_weights** :: [Type => Any] 
 
     Report the the sum of weights
+- **grid** :: [Type => Bool] 
+
+    With any of the above options (`statistic`, `npts`, `sum`) this option makes it return a grid instead of a GMTdataset.
 - **W** | **weights** :: [Type => Str | []]
 
     Unweighted input and output have 3 columns x,y,z; Weighted i/o has 4 columns x,y,z,w. Weights can
@@ -60,6 +66,8 @@ Parameters
 - $(GMT.opt_f)
 - $(GMT.opt_h)
 - $(GMT.opt_i)
+- $(GMT.opt_o)
+- $(GMT.opt_q)
 - $(GMT.opt_r)
 - $(GMT.opt_w)
 - $(GMT.opt_swap_xy)
@@ -70,13 +78,17 @@ function blockmean(cmd0::String="", arg1=nothing; kwargs...)
 
 	d = KW(kwargs)
 	help_show_options(d)		# Check if user wants ONLY the HELP mode
-	cmd = parse_these_opts("", d, [[:E :extended], [:S :statistic]])
-	if     (find_in_dict(d, [:npts :count])[1] !== nothing)  cmd *= " -Sn"
-    elseif (find_in_dict(d, [:sum])[1] !== nothing)          cmd *= " -Ss"
-    elseif (find_in_dict(d, [:sum_weights])[1] !== nothing)  cmd *= " -Sw"
-    end
+	cmd = parse_these_opts("", d, [[:S :statistic]])
+	if     (find_in_dict(d, [:npts :count])[1] !== nothing)  cmd = " -Sn"
+	elseif (find_in_dict(d, [:mean])[1] !== nothing)         cmd = " -Sm"
+	elseif (find_in_dict(d, [:sum])[1] !== nothing)          cmd = " -Ss"
+	elseif (find_in_dict(d, [:sum_weights])[1] !== nothing)  cmd = " -Sw"
+	end
+	opt_A = add_opt(d, "", 'A', [:A :field :fields], (mean="_z", std="_s", highest="_h", lowest="_l", weight="_w", weights="_w"))
+	(!occursin(" -E", cmd) && (occursin("s", opt_A) || occursin("h", opt_A) || occursin("l", opt_A))) && (opt_A *= " -E")
+	cmd *= opt_A
 
-	return common_blocks(cmd0, arg1, d, cmd, "blockmean", kwargs...)
+	common_blocks(cmd0, arg1, d, cmd, "blockmean", kwargs...)
 end
 
 # ---------------------------------------------------------------------------------------------------
@@ -93,8 +105,11 @@ function blockmedian(cmd0::String="", arg1=nothing; kwargs...)
 
 	d = KW(kwargs)
 	help_show_options(d)		# Check if user wants ONLY the HELP mode
-	cmd = parse_these_opts("", d, [[:E :extended], [:Q :quick], [:T :quantile]])
-	return common_blocks(cmd0, arg1, d, cmd, "blockmedian", kwargs...)
+	cmd = parse_these_opts("", d, [[:Q :quick], [:T :quantile]])
+	opt_A = add_opt(d, "", 'A', [:A :field :fields], (median="_z", scale="_s", highest="_h", lowest="_l", weight="_w", weights="_w"))
+	(!occursin(" -E", cmd) && (occursin("s", opt_A) || occursin("h", opt_A) || occursin("l", opt_A))) && (opt_A *= " -E")
+	cmd *= opt_A
+	common_blocks(cmd0, arg1, d, cmd, "blockmedian", kwargs...)
 end
 
 # ---------------------------------------------------------------------------------------------------
@@ -110,25 +125,37 @@ function blockmode(cmd0::String="", arg1=nothing; kwargs...)
 	length(kwargs) == 0 && return monolitic("blockmode", cmd0, arg1)
 
 	d = init_module(false, kwargs...)[1]		# Also checks if the user wants ONLY the HELP mode
-	cmd = parse_these_opts("", d, [[:E :extended], [:D :histogram_binning], [:Q :quick]])
-	return common_blocks(cmd0, arg1, d, cmd, "blockmode", kwargs...)
+	cmd = parse_these_opts("", d, [[:D :histogram_binning], [:Q :quick]])
+	opt_A = add_opt(d, "", 'A', [:A :field :fields], (mode="_z", scale="_s", highest="_h", lowest="_l", weight="_w", weights="_w"))
+	(!occursin(" -E", cmd) && (occursin("s", opt_A) || occursin("h", opt_A) || occursin("l", opt_A))) && (opt_A *= " -E")
+	cmd *= opt_A
+
+	common_blocks(cmd0, arg1, d, cmd, "blockmode", kwargs...)
 end
 
 # ---------------------------------------------------------------------------------------------------
 function common_blocks(cmd0, arg1, d, cmd, proggy, kwargs...)
 
-	cmd, = parse_common_opts(d, cmd, [:I])
-	cmd = parse_these_opts(cmd, d, [[:A :fields], [:C :center], [:W :weights]])
+	cmd = parse_these_opts(cmd, d, [[:C :center], [:E :extend :extended], [:W :weights]])
 	opt_G = parse_G(d, "")[1]
 
 	if (opt_G != "" && !occursin("-A", cmd))
-		cmd = cmd * " -Az"					# So that we can use plain -G to mean write grid 
+		cmd = cmd * " -Az"					    # So that we can use plain -G to mean write grid 
 	end
-	(length(opt_G) > 3) && (cmd *= opt_G)	# G=true will give " -G", which we'll ignore  (Have to)
-	cmd, = parse_common_opts(d, cmd, [:R :V_params :bi :di :e :f :h :i :o :r :w :yx])
+	(length(opt_G) > 3) && (cmd *= opt_G)	    # G=true will give " -G", which we'll ignore  (Have to)
+	cmd, = parse_common_opts(d, cmd, [:RIr :V_params :bi :di :e :f :h :i :o :w :yx])
 
 	cmd, _, arg1 = find_data(d, cmd0, cmd, arg1)
-	common_grd(d, proggy * " " * cmd, arg1)		# Finish build cmd and run it
+	do_grid = (proggy == "blockmean" && startswith(cmd, " -S") && find_in_dict(d, [:grid])[1] !== nothing)
+	R = common_grd(d, proggy * " " * cmd, arg1)		# Finish build cmd and run it
+	
+	(!do_grid || isa(R, String)) && return R		# Vd = 2 makes R be a string
+
+	# User asked to output the result of -S as a grid (not allowed in GMT), so call xyz2grd
+	opt_R = scan_opt(cmd, "-R")
+	opt_I = scan_opt(cmd, "-I")
+	opt_r = ((r = scan_opt(cmd, "-r")) == "") ? "g" : r
+	xyz2grd(R, R=opt_R, I=opt_I, r=opt_r)
 end
 
 # ---------------------------------------------------------------------------------------------------
