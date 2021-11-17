01efdcad4420ec0fd573d2c18ded41c6e34aae3b
Joaquim Luis <jmfluis@gmail.com>
2021-11-17 03:19:51 +0000

    Add syntax to allow passing data as x=..., y=... and z=...

--- src/xyz2grd.jl
+++ src/xyz2grd.jl
@@ -53,6 +53,9 @@ function xyz2grd(cmd0::String="", arg1=nothing; kwargs...)
 
 	cmd, = parse_common_opts(d, "", [:G :RIr :J :V_params :bi :di :e :f :h :i :w :yx])
 	cmd  = parse_these_opts(cmd, d, [[:A :multiple_nodes], [:D :header], [:S :swap], [:Z :flags]])
+	if (cmd0 == "" && arg1 === nothing)
+		(haskey(d, :x) && haskey(d, :y) && haskey(d, :z)) && (arg1 = hcat(d[:x], d[:y], d[z]))
+	end
 	common_grd(d, cmd0, cmd, "xyz2grd ", arg1)		# Finish build cmd and run it
 end
 
