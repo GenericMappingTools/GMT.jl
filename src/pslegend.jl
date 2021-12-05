56bea903f585fbf6081055c4159f2a8476942dc8
Joaquim Luis <jmfluis@gmail.com>
2021-12-05 03:41:45 +0000

    Several bug fixes

--- src/pslegend.jl
+++ src/pslegend.jl
@@ -8,9 +8,9 @@ Full option list at [`legend`]($(GMTdoc)legend.html)
 Parameters
 ----------
 
+- $(GMT.opt_B)
 - $(GMT.opt_J)
 - $(GMT.opt_R)
-- $(GMT.opt_B)
 - **C** | **clearance** :: [Type => Str]
 
     Sets the clearance between the legend frame and the internal items [4p/4p].
@@ -23,6 +23,18 @@ Parameters
 
     Without further options, draws a rectangular border around the legend using *MAP_FRAME_PEN*.
     ($(GMTdoc)legend.html#f)
+- **M** :: [Type => Bool]
+
+    Modern mode only:
+    ($(GMTdoc)legend.html#m)
+- **S** | **scale** :: [Type => Number]
+
+    Scale all symbol sizes by a common scale
+    ($(GMTdoc)legend.html#s)
+- **T** | **leg_file** :: [Type => Str]
+
+    Modern mode only: Write hidden legend specification file to fname.
+    ($(GMTdoc)legend.html#t)
 - $(GMT.opt_Jz)
 - $(GMT.opt_P)
 - $(GMT.opt_U)
@@ -30,8 +42,8 @@ Parameters
 - $(GMT.opt_X)
 - $(GMT.opt_Y)
 - $(GMT.opt_p)
+- $(GMT.opt_q)
 - $(GMT.opt_t)
-- $(GMT.opt_swap_xy)
 """
 function legend(cmd0::String="", arg1=nothing; first=true, kwargs...)
 
@@ -41,15 +53,16 @@ function legend(cmd0::String="", arg1=nothing; first=true, kwargs...)
 	d, K, O = init_module(first, kwargs...)		# Also checks if the user wants ONLY the HELP mode
 
 	cmd, _, _, opt_R = parse_BJR(d, "", "", O, " -JX12c/0")
-	cmd, = parse_common_opts(d, cmd, [:F :c :p :t :JZ :UVXY :params], first)
+	cmd, = parse_common_opts(d, cmd, [:F :c :p :q :t :JZ :UVXY :params], first)
 
 	# If file name sent in, read it and compute a tight -R if this was not provided 
 	cmd, arg1, opt_R, = read_data(d, cmd0, cmd, arg1, opt_R)
 
 	cmd = parse_type_anchor(d, cmd, [:D :pos :position],
-	                        (map=("g", arg2str, 1), inside=("j", nothing, 1), norm=("n", arg2str, 1), paper=("x", arg2str, 1), anchor=("", arg2str, 2), width=("+w", arg2str), justify="+j", spacing="+l", offset=("+o", arg2str)), 'j')
-	cmd = add_opt(d, cmd, 'C', [:C :clearance])
+	                        (map=("g", arg2str, 1), outside=("J", arg2str, 1), inside=("j", arg2str, 1), norm=("n", arg2str, 1), paper=("x", arg2str, 1), anchor=("", arg2str, 2), width=("+w", arg2str), justify="+j", spacing="+l", offset=("+o", arg2str)), 'j')
+	cmd  = parse_these_opts(cmd, d, [[:C :clearance], [:M], [:S :scale], [:T :leg_file]])
 
+	show_kwargs[1] && legend_help()
 	isa(arg1, NamedTuple) && (arg1 = text_record(mk_legend(arg1)))
 	r = finish_PS_module(d, gmt_proggy * cmd, "", K, O, true, arg1)
 	gmt("destroy")
@@ -58,24 +71,28 @@ end
 
 #=
 GMT.mk_legend(gap="-0.1i", header=(text="My Map Legend", font=(24,"Times-Roman")), hline=(pen=1, offset="0.2i"), ncolumns=2, vline=(pen=1, offset=0), symbol=(marker=:circ, size="0.15i", dx_left="0.1i", fill="p300/12", dx_right="0.3i", text="This circle is hachured"), Symbol1=(marker=:ellipse, size="0.15i", dx_left="0.1i", fill=:yellow, dx_right="0.3i", text="This ellipse is yellow"), Symbol2=(marker=:wedge, size="0.15i", dx_left="0.1i", fill=:green, dx_right="0.3i", text="This wedge is green"), hline2=(pen=1, offset="0.2i"), ncolumns2=2, map_scale=(lon=5,lat=5,length="600+u+f"), gap2="0.05i", image=(width="3i", fname="@SOEST_block4.png",justify=:CT), gap3="0.05i", label=(txt="Smith et al., @%5%J. Geophys. Res., 99@%%", justify=:R, font=(9, "Times-Roman")), gap4="0.1i", text1="Let us just try some simple text that can go on a few lines.", text2="There is no easy way to predetermine how many lines may be required")
-
-println("  header=(text=?(a text) [, font=?(text font)])  (H) --> Plots a centered text string")
-println("  cmap|cpt=?(CPT file name)  (A) --> Symbol or cell color fills may be given indirectly via a z-value")
-println("  colorbar=(name=?(CPT file name), offset=?, height=? [, extra=?(extra opts)])  (B) --> Plot horizontal colorbar")
-println("  textcolor=?(color)  (C) --> Color with which the remaining text is to be printed via z=value")
-println("  hline=(pen=?(pen)[, offset=?])  (D) --> Horizontal line with specified pen across the legend")
-println("  fill=?(color))  --> (F) Fill (color of pattern) for cells")
-println("  gap|vspace=?(amount))  --> (G) Specifies a vertical gap of the given length")
-println("  image=(name=?(img file name), width=?, justify=?(2 char code))  --> (I) Place image justified relative to the current point")
-println("  label=(text=?(a text), justify=?(L|R|C)[, font=?(text font)])  --> (L) Plots a (L)eft, (C)entered, or (R)ight-justified text")
-println("  map_scale=([lon=?,] lat|y=?(lat of scale), length=? [box=?(map panel), region=?, proj=?])  --> (M) Place a map scale")
-println("  ncol[xx]=?(number of columns)  --> (N) Change the number of columns in the legend")
-println("  paragraph=?(Bool or a String)  --> (P) Start a new text paragraph")
-println("  symbol[xx]=([dx_left=?,] marker=?(symbol name), size=?(symbol size), [fill=?(color), pen=?(outline)] [, dx_right=?, label=?(text)])  --> (S) Plots the selected symbol")
-println("  text[xx]=?(the text)  (T) --> One or more of these records with paragraph-text")
-println("  vline=(pen=?(pen)[, offset=?])  (V) --> Draws a vertical line between columns")
-
 =#
+
+function legend_help(test::Bool=false)
+	io = (!test) ? stdout : IOBuffer()
+	println(io, "\n\tLegend Codes\n")
+	println(io, "  header=(text=?(a text) [, font=?(text font)])  (H) --> Plots a centered text string")
+	println(io, "  cmap|cpt=?(CPT file name)  (A) --> Symbol or cell color fills may be given indirectly via a z-value")
+	println(io, "  colorbar=(name=?(CPT file name), offset=?, height=? [, extra=?(extra opts)])  (B) --> Plot horizontal colorbar")
+	println(io, "  textcolor=?(color)  (C) --> Color with which the remaining text is to be printed via z=value")
+	println(io, "  hline=(pen=?(pen)[, offset=?])  (D) --> Horizontal line with specified pen across the legend")
+	println(io, "  fill=?(color))  --> (F) Fill (color of pattern) for cells")
+	println(io, "  gap|vspace=?(amount))  --> (G) Specifies a vertical gap of the given length")
+	println(io, "  image=(name=?(img file name), width=?, justify=?(2 char code))  --> (I) Place image justified relative to the current point")
+	println(io, "  label=(text=?(a text), justify=?(L|R|C)[, font=?(text font)])  --> (L) Plots a (L)eft, (C)entered, or (R)ight-justified text")
+	println(io, "  map_scale=([lon=?,] lat|y=?(lat of scale), length=? [box=?(map panel), region=?, proj=?])  --> (M) Place a map scale")
+	println(io, "  ncol[xx]=?(number of columns)  --> (N) Change the number of columns in the legend")
+	println(io, "  paragraph=?(Bool or a String)  --> (P) Start a new text paragraph")
+	println(io, "  symbol[xx]=([dx_left=?,] marker=?(symbol name), size=?(symbol size), [fill=?(color), pen=?(outline)] [, dx_right=?, label=?(text)])  --> (S) Plots the selected symbol")
+	println(io, "  text[xx]=?(the text)  (T) --> One or more of these records with paragraph-text")
+	println(io, "  vline=(pen=?(pen)[, offset=?])  (V) --> Draws a vertical line between columns")
+end
+
 mk_legend(nt::NamedTuple) = mk_legend(; nt...)
 function mk_legend(; kwargs...)
 	leg::Vector{String} = Vector{String}(undef, length(kwargs))
@@ -91,16 +108,16 @@ function mk_legend(; kwargs...)
 		code = isa(kwargs[n], NamedTuple) ? kwargs[n] : NamedTuple([ky[n]] .=> [kwargs[n]])
 		kw_str = lowercase(string(ky[n]))
 		k = keys(code)
-		if (startswith(kw_str, "header"))		# code = (header=txt, font=?)	H 24p,Times-Roman My Map Legend
+		if (kw_str == "H" || startswith(kw_str, "header"))		# code = (header=txt, font=?)	H 24p,Times-Roman My Map Legend
 			d = nt2dict(code)
 			f::String = ((val = find_in_dict(d, [:font])[1]) === nothing) ? "-" : font(val)
 			leg[n] = "H " * f * " " * string(d[Symbol.(keys(d))[1]])
 			check_unused(d, kw_str, Symbol.(keys(d))[1])
 
-		elseif (startswith(kw_str, "cpt") || startswith(kw_str, "cmap"))
+		elseif (kw_str == "A" || startswith(kw_str, "cpt") || startswith(kw_str, "cmap"))
 			leg[n] = "A " * string(code[1])
 
-		elseif (startswith(kw_str, "colorbar"))	# code = (name="tt.cpt", offset=0.5, height=0.5) B tt.cpt 0.2i 0.2i -B0
+		elseif (kw_str == "B" || startswith(kw_str, "colorbar"))	# code = (name="tt.cpt", offset=0.5, height=0.5)
 			d = nt2dict(code)
 			((val = find_in_dict(d, [:offset])[1]) === nothing) && error("Must specify the 'offset' in 'colorbar'")
 			f = string(val)
@@ -110,22 +127,22 @@ function mk_legend(; kwargs...)
 			leg[n] = "B " * string(d[Symbol.(keys(d))[1]]) * " " * f
 			check_unused(d, kw_str, Symbol.(keys(d))[1])
 
-		elseif (startswith(kw_str, "textcolor"))
+		elseif (kw_str == "C" || startswith(kw_str, "textcolor"))
 			leg[n] = "C " * get_color(code[1])
 
-		elseif (startswith(kw_str, "hline"))	# code = (hline=pen, offset=?)	D 0.2i 1p
+		elseif (kw_str == "D" || startswith(kw_str, "hline"))	# code = (hline=pen, offset=?)	D 0.2i 1p
 			d = nt2dict(code)
 			f = ((val = find_in_dict(d, [:offset])[1]) === nothing) ? "" : string(val)
-			leg[n] = "D " * f * " " * add_opt_pen(d, [:pen :header], "", true)
+			leg[n] = "D " * f * " " * add_opt_pen(d, [:pen], "", true)
 			check_unused(d, kw_str)
 
-		elseif (startswith(kw_str, "fill"))
+		elseif (kw_str == "F" || startswith(kw_str, "fill"))
 			leg[n] = "F " *  join([@sprintf("%s ", get_color(x)) for x in code])
 
-		elseif (startswith(kw_str, "vspace") || startswith(kw_str, "gap"))	# code = (vspace=val)	G -0.1i
+		elseif (kw_str == "G" || startswith(kw_str, "vspace") || startswith(kw_str, "gap"))	# code = (vspace=val)	G -0.1i
 			leg[n] = "G " * string(code[1])
 
-		elseif (startswith(kw_str, "image"))	# code = (image=fname)	I @SOEST_block4.png 3i CT
+		elseif (kw_str == "I" || startswith(kw_str, "image"))	# code = (image=fname)	I @SOEST_block4.png 3i CT
 			d = nt2dict(code)
 			((val = find_in_dict(d, [:width])[1]) === nothing) && error("Must specify the 'width' in 'image'")
 			f = string(val)
@@ -134,15 +151,15 @@ function mk_legend(; kwargs...)
 			leg[n] = "I " * string(d[Symbol.(keys(d))[1]]) * " " * f
 			check_unused(d, kw_str, Symbol.(keys(d))[1])
 
-		elseif (startswith(kw_str, "label"))	# code = L 9p,Times-Roman R Smith et al., @%5%J. Geophys. Res., 99@%%, 2000
+		elseif (kw_str == "L" || startswith(kw_str, "label"))	# code = L 9p,Times-Roman R Smith et al., @%5%J. Geophys. Res., 99@%%, 2000
 			d = nt2dict(code)
 			((val = find_in_dict(d, [:justify :justification])[1]) === nothing) && error("Must specify the 'justify' in 'label'")
 			f = string(val)
-			f = ((val = find_in_dict(d, [:font])[1]) === nothing) ? "-" : font(val) * " " * f
+			f = (((val = find_in_dict(d, [:font])[1]) === nothing) ? "-" : font(val)) * " " * uppercase(f[1])
 			leg[n] = "L " * f * " " * string(d[Symbol.(keys(d))[1]])
 			check_unused(d, kw_str, Symbol.(keys(d))[1])
 
-		elseif (occursin("scale", kw_str))		# code = (map_scale=)	M 5 5 600+u+f
+		elseif (kw_str == "M" || occursin("scale", kw_str))		# code = (map_scale=)	M 5 5 600+u+f
 			d = nt2dict(code)
 			f = ((val = find_in_dict(d, [:lon :x])[1]) === nothing) ? "-" : string(val)
 			((val = find_in_dict(d, [:lat :y])[1]) === nothing) && error("Must specify the 'lat or y' in map_scale")
@@ -155,21 +172,22 @@ function mk_legend(; kwargs...)
 			leg[n] *= opt_F * opt_R * opt_J
 			check_unused(d, kw_str)
 
-		elseif (startswith(kw_str, "ncol"))		# code = (ncolumns=?)	N 2
-			leg[n] = "N " * arg2str(code[1])
+		elseif (kw_str == "N" || startswith(kw_str, "ncol"))		# code = (ncolumns=?)	N 2
+			leg[n] = "N " * string(code[1])
 
-		elseif (startswith(kw_str, "parag"))	# NOTE: Pargraph also accepts options like pstext -M but that's too complicated to parse
+		elseif (kw_str == "P" || startswith(kw_str, "parag"))
+			# NOTE: Pargraph also accepts options like pstext -M but that's too complicated to parse
 			t = (isa(code[1], Bool) && code[1]) ? "" : string(code[1])
 			leg[n] = "P " * t
 
-		elseif (startswith(kw_str, "symb"))
+		elseif (kw_str == "S" || startswith(kw_str, "symb"))
 			# code = (symbol=:circ, size=0.15, dx_left=0.1, fill="p300/12", dx_righ=0.3, text="This circ")
 			# S [dx1 symbol size fill pen [ dx2 text ]]
 			d = nt2dict(code)
 			marca::String = get_marker_name(d, nothing, [:symbol, :marker], false, true)[1]
 			f = ((val = find_in_dict(d, [:dx_left])[1]) === nothing) ? "- " : string(val, " ");	dx1 = f
 			f = ((val = find_in_dict(d, [:fill])[1]) === nothing) ? "- " : get_color(val);			fill = f
-			f = add_opt_pen(d, [:pen], "", true);	pen = (f == "") ? " -" : f		# TRUE to also seek (lw,lc,ls)
+			f = add_opt_pen(d, [:pen], "", true);	pen = (f == "") ? " -" : " " * f		# TRUE to also seek (lw,lc,ls)
 			((val = find_in_dict(d, [:size])[1]) === nothing) && error("Must specify the 'size' in 'symbol'")
 			_size = arg2str(val)
 			f = ((val = find_in_dict(d, [:label :text])[1]) === nothing) ? "" : string(val);	label = f
@@ -180,13 +198,13 @@ function mk_legend(; kwargs...)
 			leg[n] = "S " * dx1 * marca * " " * _size * " " * fill * pen * label
 			check_unused(d, kw_str)
 
-		elseif (startswith(kw_str, "text"))
+		elseif (kw_str == "T" || startswith(kw_str, "text"))
 			leg[n] = "T " * string(code[1])
 
-		elseif (startswith(kw_str, "vline"))		# code = (vline=pen, offset=?)	V 0 1p
+		elseif (kw_str == "V" || startswith(kw_str, "vline"))		# code = (vline=pen, offset=?)	V 0 1p
 			d = nt2dict(code)
 			f = ((val = find_in_dict(d, [:offset])[1]) === nothing) ? "" : string(val)
-			leg[n] = "V " * f * " " * add_opt_pen(d, [:pen :header], "", true)
+			leg[n] = "V " * f * " " * add_opt_pen(d, [:pen], "", true)
 			check_unused(d, kw_str)
 
 		else
