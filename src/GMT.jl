e3afa1f6a9ac9f46bb2473dd673103c9b6784464
Joaquim Luis <jmfluis@gmail.com>
2022-05-09 17:45:16 +0100

    Test that the minimum GMT required version is available.

--- src/GMT.jl
+++ src/GMT.jl
@@ -27,6 +27,7 @@ function get_GMTver()
 		(get(ENV, "FORCE_INSTALL_GMT", "") != "") && error("Forcing an automatic GMT install")
 		ver = readlines(`gmt --version`)[1]
 		out = ((ind = findfirst('_', ver)) === nothing) ? VersionNumber(ver) : VersionNumber(ver[1:ind-1])
+		(out < v"6.1") && error("Need at least GMT6.1. The one you have ($out) is not supported.")
 		return out, false, "", "", "", ""
 	catch err1;		println(err1)		# If not, install GMT
 		ENV["INSTALL_GMT"] = "1"
