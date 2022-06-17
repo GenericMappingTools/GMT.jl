89f18b857822044f853a6e35fbc80dd249003a80
Joaquim Luis <jmfluis@gmail.com>
2022-06-17 02:52:11 +0100

    And add another

--- src/common_options.jl
+++ src/common_options.jl
@@ -1543,6 +1543,7 @@ end
 
 # ---------------------------------------------------------------------------------------------------
 parse_pen(pen::Real)::String = string(pen)
+parse_pen(pen::String)::String = pen
 function parse_pen(pen::Tuple)::String
 	# Convert an empty to 3 args tuple containing (width[c|i|p]], [color], [style[c|i|p|])
 	s = arg2str(pen[1])					# First arg is different because there is no leading ','
