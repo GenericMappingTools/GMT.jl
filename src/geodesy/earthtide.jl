107d89b7e9e692f7d374942e6fe33f897e885c12
Joaquim Luis <jmfluis@gmail.com>
2022-07-06 14:52:48 +0100

    When outputting a dataset, set the column names and the Timecol attrib.

--- src/geodesy/earthtide.jl
+++ src/geodesy/earthtide.jl
@@ -13,13 +13,18 @@ Full option list at [`earthtide`]($(GMTdoc)earthtide.html)
 function earthtide(cmd0::String=""; kwargs...)
 
 	d = init_module(false, kwargs...)[1]		# Also checks if the user wants ONLY the HELP mode
-	
+
 	cmd = "earthtide " * parse_common_opts(d, "", [:R :I :V_params :r])[1]
 	cmd = parse_opt_range(d, cmd, "T")[1]
 	if ((opt_S = add_opt(d, "", "S", [:S :sun_moon])) != "")
+		(dbg_print_cmd(d, cmd) !== nothing) && return cmd * opt_S
 		return finish_PS_module(d, cmd * opt_S, "", true, false, false)
 	elseif ((opt_L = add_opt(d, "", "L", [:L :location])) != "")
-		return finish_PS_module(d, cmd * opt_L, "", true, false, false)
+		(dbg_print_cmd(d, cmd) !== nothing) && return cmd * opt_L
+		R = finish_PS_module(d, cmd * opt_L, "", true, false, false)
+		R.colnames = ["Time", "East", "North", "Vertical"]
+		R.attrib = Dict("Timecol" => "1")
+		return R
 	end
 
 	cmd = ((opt_C = add_opt(d, "", "C", [:C :components])) != "") ? cmd * opt_C : cmd * " -Cz"
