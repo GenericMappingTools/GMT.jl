68b762ee09c74d6e526ed5d8ffe85cb613f72f05
Joaquim Luis <jmfluis@gmail.com>
2022-08-28 01:42:22 +0100

    Let geodesic_long() effectively use the 'proj' keyword.

--- src/gdal_utils.jl
+++ src/gdal_utils.jl
@@ -666,10 +666,10 @@ end
 
 # ---------------------------------------------------------------------------------------------------
 """
-    xy2lonlat(xy::Matrix{<:Real}; s_srs, t_srs="+proj=longlat +datum=WGS84")
+    xy2lonlat(xy::Matrix{<:Real}, s_srs=""; s_srs="", t_srs="+proj=longlat +datum=WGS84")
 or
 
-    xy2lonlat(D::GMTdataset; s_srs, t_srs="+proj=longlat +datum=WGS84")
+    xy2lonlat(D::GMTdataset, s_srs=""; s_srs="", t_srs="+proj=longlat +datum=WGS84")
 
 Computes the inverse projection from XY to LonLat in the given projection. The output is assumed to be in WGS84.
 If that isn't right, pass the appropriate projection info via the `t_srs` option (PROJ4, WKT, EPSG).
