43ba822ec711db0eb1cf297e0abe6f86191dcc05
Joaquim Luis <jmfluis@gmail.com>
2022-09-23 18:19:43 +0100

    Doc strings work

--- src/utils_types.jl
+++ src/utils_types.jl
@@ -1286,27 +1286,27 @@ mksymbol(f::Function, arg1; kw...) = mksymbol(f, "", arg1; kw...)
 
 # ---------------------------------------------------------------------------------------------------
 """
-    polygonlevels(D::GDtype, ids::Vector{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
+    zvals = polygonlevels(D::GDtype, ids::Vector{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
 or
 
-    polygonlevels(D::GDtype, ids::Matrix{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
+    zvals = polygonlevels(D::GDtype, ids::Matrix{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
 
-Creates a vector with ZVALS to use in ``plot`` and where length(ZVALS) == length(D)
-The elements of ZVALS are made up from the `vals`.
+Creates a vector with `zvals` to use in `plot` and where length(zvals) == length(D)
+The elements of `zvals` are made up from the `vals`.
 
-- `ids`:    is a string Vector or Matrix with the ids (attrinute names) of the GMTdataset D.
-            If a Matrix (2 columns only) then the `att` bellow must also have two names (string vector
-            with two elements) that will be matched against the two eements of each line of `user_ids`.
+- `ids`:    is a string Vector or Matrix with the ids (attribute names) of the GMTdataset D.
+            If a Matrix (2 columns only) then the `att` bellow must also have the two names (string vector
+            with two elements) that will be matched against the two elements of each line of `user_ids`.
             The idea here is to match two conditions: `att[1] == ids[n,1] && att[2] == ids[n,2]`
-- `vals`:      is a vector with the numbers to be used in plot `level` to color the polygons.
-- `attrib` or `att`: keyword to selecect which attribute to use when matching with contents of the `ids` strings.
+- `vals`:   is a vector with the numbers to be used in plot `level` to color the polygons.
+- `attrib` or `att`: keyword to select which attribute to use when matching with contents of the `ids` strings.
 - `nocase` or `insensitive`: a keyword from `kw`. Perform a case insensitive comparision between the contents of
                `ids` and the attribute specified with `attrib`. Default compares as case sensistive.
 - `repeat`: keyword to replicate the previously known value until it finds a new segment ID for the case
-            when a polygon have no attributes (may happen for the islands of country).
+            when a polygon have no attributes (may happen for the islands in a country).
 
 Returns a Vector{Float64} with the same length as the number of segments in D. Its content are
-made up after the contents of `vals` but may be repeated such that each polygon of the same family, i.e.
+made up from the contents of `vals` but may be repeated such that each polygon of the same family, i.e.
 with the same `ids`, has the same value.
 """
 function polygonlevels(D::Vector{<:GMTdataset}, user_ids::Vector{String}, vals::Vector{<:Real}; kw...)::Vector{Float64}
