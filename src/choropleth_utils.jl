074cf99430ba1b39c2b26cc3c219e7439a878bea
Joaquim Luis <jmfluis@gmail.com>
2022-10-05 02:46:52 +0100

    Typos in docstrings

--- src/choropleth_utils.jl
+++ src/choropleth_utils.jl
@@ -3,9 +3,9 @@
 
 Create a categorical CPT to use with the output of `coast(dcw=...)` to make Choropleth maps.
   - `codes` is a comma separated string with two chars country codes (ex: "PT,ES,FR,IT")
-  - `vals` a vector with same size as number of country codes, with the values used to colorize the countries
-Optionally provide a CPT in the kwarg `cmap=CPT` with a range sufficient to tansform the `vals` in colors.
-As an alternative to the above provide a `makecpt` type `range` numeric vector to create a CPT.
+  - `vals` a vector with same size as country codes, with the values used to colorize the countries
+Optionally provide a CPT in the kwarg `cmap=CPT` with a range sufficient to transform the `vals` in colors.
+As an alternative to the above, provide a `makecpt` type `range` numeric vector to create a CPT.
 If none of these are provided a default `CPT = makecpt(range=(0,255,1))` will be used.
 """
 cpt4dcw(codes::String, vals::Vector{<:Real}; kwargs...) = cpt4dcw(split(codes,","), vals; kwargs...)
