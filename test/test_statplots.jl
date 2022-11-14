2d7d6b5c56666b6a74b5905b422e3d8a6309c045
Joaquim Luis <jmfluis@gmail.com>
2022-11-14 02:23:17 +0000

    Some fixes in the show separator option.

--- test/test_statplots.jl
+++ test/test_statplots.jl
@@ -18,4 +18,15 @@
 	violin(y, G=true, split=true)
 	violin(y, G=true, split=true, boxplot=true)
 	violin(y, G=true, scatter=true, boxplot=true)
+
+	vv = [round.(randn(50),digits=1), round.(randn(40),digits=3)]
+	GMT.kernelDensity(vv)
+
+	vvv = [[randn(50), randn(30)], [randn(40), randn(48), randn(45)], [randn(35), randn(43)]];
+	GMT.kernelDensity(vvv)
+	boxplot(mat2ds(randn(20)), Vd=dbg2)
+	violin(vvv, fill=true, boxplot=true, separator=true, scatter=true)
+	boxplot(vvv, fill=true, separator=true)
+	violin(randn(50), rand(1:3,50), Vd=dbg2)
+	boxplot(randn(50), rand(1:3,50), Vd=dbg2)
 end
