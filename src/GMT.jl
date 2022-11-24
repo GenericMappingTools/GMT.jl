bd7f92e205d5ca6b9fc97eab7b52901e14c09c13
Joaquim Luis <jmfluis@gmail.com>
2022-11-24 01:52:05 +0000

    Export parallelplot!

--- src/GMT.jl
+++ src/GMT.jl
@@ -167,7 +167,8 @@ export
 
 	add2PSfile, append2fig, regiongeog, streamlines, wmsinfo, wmstest, wmsread, polygonlevels,
 
-	density, density!, boxplot, boxplot!, ecdfplot, ecdfplot!, parallelplot, qqplot, qqplot!, qqnorm, qqnorm!, violin, violin!
+	density, density!, boxplot, boxplot!, ecdfplot, ecdfplot!, parallelplot, parallelplot!, qqplot, qqplot!,
+	qqnorm, qqnorm!, violin, violin!
 
 include("common_docs.jl")
 include("libgmt_h.jl")
