bade9852443d097f622fbd51e66428e89a5f7ea8
Joaquim Luis <jmfluis@gmail.com>
2022-12-20 18:10:34 +0000

    Export the fill_between functions.

--- src/GMT.jl
+++ src/GMT.jl
@@ -168,8 +168,8 @@ export
 
 	add2PSfile, append2fig, regiongeog, streamlines, wmsinfo, wmstest, wmsread, polygonlevels,
 
-	density, density!, boxplot, boxplot!, cornerplot, cornerplot!, ecdfplot, ecdfplot!, marginalhist, marginalhist!,
-	parallelplot, parallelplot!, qqplot, qqplot!, qqnorm, qqnorm!, violin, violin!
+	density, density!, boxplot, boxplot!, cornerplot, cornerplot!, ecdfplot, ecdfplot!, fill_between, fill_between!,
+	marginalhist, marginalhist!, parallelplot, parallelplot!, qqplot, qqplot!, qqnorm, qqnorm!, violin, violin!
 
 include("common_docs.jl")
 include("libgmt_h.jl")
