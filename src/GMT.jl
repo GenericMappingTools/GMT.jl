98b3528b1bbffb6a9cf7152560233f04370293c3
Joaquim Luis <jmfluis@gmail.com>
2022-12-23 00:11:18 +0000

    Export the macro @?

--- src/GMT.jl
+++ src/GMT.jl
@@ -169,7 +169,8 @@ export
 	add2PSfile, append2fig, regiongeog, streamlines, wmsinfo, wmstest, wmsread, polygonlevels,
 
 	density, density!, boxplot, boxplot!, cornerplot, cornerplot!, ecdfplot, ecdfplot!, fill_between, fill_between!,
-	marginalhist, marginalhist!, parallelplot, parallelplot!, qqplot, qqplot!, qqnorm, qqnorm!, violin, violin!
+	marginalhist, marginalhist!, parallelplot, parallelplot!, qqplot, qqplot!, qqnorm, qqnorm!, violin, violin!,
+	@?
 
 include("common_docs.jl")
 include("libgmt_h.jl")
