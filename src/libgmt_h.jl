0c204344b1e2ca30120a9e375139d722ba7e7e08
Joaquim Luis <jmfluis@gmail.com>
2023-02-14 19:43:42 +0000

    Wrap the (part of) API struct when using GMT 6.5

--- src/libgmt_h.jl
+++ src/libgmt_h.jl
@@ -292,18 +292,25 @@ struct GMTAPI_DATA_OBJECT
 	ID::UInt32					# Unique identifier which is >= 0
 	alloc_level::UInt32			# Nested module level when object was allocated
 	status::UInt32				# 0 when first registered, 1 after reading/writing has started, 2 when finished
-	selected::Cint				# true if requested by current module, false otherwise
-	close_file::Cint			# true if we opened source as a file and thus need to close it when done
-	region::Cint				# true if wesn was passed, false otherwise
-	no_longer_owner::Cint		# true if the data pointed to by the object was passed on to another object
-	messenger::Cint				# true for output objects passed from the outside to receive data from GMT. If true we destroy data pointer before writing
+	orig_pad::NTuple{4,UInt32}	# Original grid pad
+	reset_pad::UInt32			# 1 for input memory grids from which a subregion was requested
+	h_delay::Bool				# We must delay writing table headers until memory allocated
+	s_delay::Bool				# We must delay writing segment header until memory allocated
+	selected::Bool				# true if requested by current module, false otherwise
+	close_file::Bool			# true if we opened source as a file and thus need to close it when done
+	region::Bool				# true if wesn was passed, false otherwise
+	no_longer_owner::Bool		# true if the data pointed to by the object was passed on to another object
+	messenger::Bool				# true for output objects passed from the outside to receive data from GMT. If true we destroy data pointer before writing
+	module_input::Bool			# true for input objects that will serve as module input(s) and not option inputs */
 	alloc_mode::UInt32			# GMT_ALLOCATED_{BY_GMT|EXTERNALLY}
 	direction::UInt32			# GMT_IN or GMT_OUT
 	family::UInt32				# One of GMT_IS_{DATASET|TEXTSET|CPT|IMAGE|GRID|MATRIX|VECTOR|COORD}
 	actual_family::UInt32		# May be GMT_IS_MATRIX|VECTOR when one of the others are created via those
+	type::UInt32				# Desired output array type for auto-allocated VECTOR and MATRIX
 	method::UInt32              # One of GMT_IS_{FILE,STREAM,FDESC,DUPLICATE,REFERENCE} or sum with enum GMT_enum_via (GMT_VIA_{NONE,VECTOR,MATRIX,OUTPUT}); using unsigned type because sum exceeds enum GMT_enum_method
 	geometry::UInt32			# One of GMT_IS_{POINT|LINE|POLY|PLP|SURFACE|NONE}
-	wesn::NTuple{4,Cdouble}		# Grid domain limits
+	wesn::NTuple{6,Cdouble}		# Active Grid domain limits
+	orig_wesn::NTuple{6,Cdouble}# Original Grid domain limits
 	resource::Ptr{Cvoid}		# Points to registered filename, memory location, etc., where data can be obtained from with GMT_Get_Data.
 	data::Ptr{Cvoid}			# Points to GMT object that was read from a resource
 	# FILE *fp;					# Pointer to source/destination stream [For rec-by-rec procession, NULL if memory location]
@@ -314,11 +321,12 @@ struct GMTAPI_DATA_OBJECT
 	# Start of temporary variables for API debug - They are only set when building GMT with /DEBUG
 	G::Ptr{Cvoid}				# struct GMT_GRID *G;
 	D::Ptr{Cvoid}				# struct GMT_DATASET *D;
-	T::Ptr{Cvoid}				# struct GMT_TEXTSET *T;
 	C::Ptr{Cvoid}				# struct GMT_PALETTE *C;
+	P::Ptr{Cvoid}				# struct GMT_POSTSCRIPT *P;
 	M::Ptr{Cvoid}				# struct GMT_MATRIX *M;
 	V::Ptr{Cvoid}				# struct GMT_VECTOR *V;
 	I::Ptr{Cvoid}				# struct GMT_IMAGE *I;
+	U::Ptr{Cvoid}				# struct GMT_CUBE *U;
 end
 
 struct Gmt_libinfo
@@ -371,66 +379,98 @@ struct COMMON_R
 	string::NTuple{256,UInt8}
 end
 
-#= 
-struct GMTAPI_CTRL
-	# Master controller which holds all GMT API related information at run-time for a single session.
-	# Users can run several GMT sessions concurrently; each session requires its own structure.
-	# Use GMTAPI_Create_Session to initialize a new session and GMTAPI_Destroy_Session to end it.
+#=
+struct GMT_INTERNAL
+	func_level::UInt32				# Keeps track of what level in a nested GMT_func calling GMT_func etc we are.
+	mem_set::Bool					# true when we have initialized the tmp memory already */
+	sample_along_arc::Bool			# true when sample1d need exact sampling along the arc */
+	mem_cols::Csize_t				# Current number of allocated columns for temp memory */
+	mem_rows::Csize_t				# Current number of allocated rows for temp memory */
+	mem_txt_alloc::Csize_t
+	mem_txt_dup::Csize_t
+	mem_coord::Ptr{Ptr{Cdouble}}	# Columns of temp memory */
+	mem_txt::Ptr{Ptr{UInt8}}		# For temp text */
+end
 
-	current_rec::NTuple{2,UInt64}	# Current record number >= 0 in the combined virtual dataset (in and out)
-	n_objects::UInt32			# Number of currently active input and output data objects
-	unique_ID::UInt32			# Used to create unique IDs for duration of session
-	session_ID::UInt32			# ID of this session
-	unique_var_ID::UInt32		# Used to create unique object IDs (grid,dataset, etc) for duration of session
-	current_item::NTuple{2,Cint}	# Array number of current dataset being processed (in and out)
-	pad::UInt32					# Session default for number of rows/cols padding for grids [2]
-	external::UInt32			# 1 if called via external API (MATLAB, Python) [0]
-	runmode::UInt32				# nonzero for GMT modern runmode [0 = classic]
-	shape::Cint                 # GMT_IS_COL_FORMAT (1) if column-major (MATLAB, Fortran), GMT_IS_ROW_FORMAT (0) if row-major
-	leave_grid_scaled::UInt32	# 1 if we dont want to unpack a grid after we packed it for writing [0]
-	n_cores::UInt32             # Number of available cores on this system
-	n_tmp_headers::Cint         # Number of temporarily held table headers
-	terminal_width::Cint        # Width of the terminal
-	verbose::UInt32             # Used until GMT is set up
-	registered::NTuple{2,Bool}	# true if at least one source/destination has been registered (in and out)
-	io_enabled::NTuple{2,Bool}	# true if access has been allowed (in and out)
-	module_input::Bool          # true when we are about to read inputs to the module (command line) */
-	usage::Bool                 # Flag when 1-liner modern mode modules just want usage
-	allow_reuse::Bool           # Flag when get_region_from_data can read a file and not flag it as "used"
-	is_file::Bool               # True if current rec-by-rec i/o is from a physical file
-	cache::Bool                 # true if we want to read a cache file via GDAL
-	no_history::Bool            # true if we want to disable the gmt.history mechanism
-	got_remote_wesn::Bool       # true if we obtained w/e/sn via a remote grid/image with no resolution given
-	use_gridline_registration::Bool       # true if default remote grid registration should be gridline, not pixel
-	use_gridline_registration_warn::Bool  # true if we should warn about the above
-	n_objects_alloc::Csize_t	# Allocation counter for data objects
-	error::Int32				# Error code from latest API call [GMT_OK]
-	last_error::Int32			# Error code from previous API call [GMT_OK]
-	shelf::Int32				# Place to pass hidden values within API
-	log_level::UInt32           # 0 = stderr, 1 = just this module, 2 = set until unset
-	io_mode::NTuple{2,UInt32}	# 1 if access as set, 0 if record-by-record
-	tile_wesn::NTuple{GMTAPI_N_GRID_ARGS,Cdouble}  # Original region used when getting tiles (perhaps result of -Roblique -J)
-	tile_inc::Cdouble           # Remote grid increment in degrees
-	tile_reg::UInt8             # Remote grid registration */
-	#GMT::Ptr{GMT_CTRL}			# Key structure with low-level GMT internal parameters
-	GMT::Ptr{Cvoid}				# Maybe one day. Till than just keep it as Cvoid
-	object::Ptr{Ptr{GMTAPI_DATA_OBJECT}}	# List of registered data objects
-	session_tag::Ptr{UInt8}		# Name tag for this session (or NULL)
-	session_name::Ptr{UInt8}    # Unique name for modern mode session (NULL for classic) */
-	tmp_dir::Ptr{UInt8}         # System tmp_dir (NULL if not found)
-	session_dir::Ptr{UInt8}     # GMT Session dir (NULL if not running in modern mode)
-	gwf_dir::Ptr{UInt8}         # GMT WorkFlow dir (NULL if not running in modern mode)
-	tmp_header::Ptr{Ptr{UInt8}} # Temporary table headers held until we are able to write them to destination */
-	tmp_segmentheader::Ptr{UInt8} # Temporary segment header held until we are able to write it to destination */
-	message::Ptr{UInt8}         # To be allocated by Create_Session and used for messages */
-	error_msg::NTuple{4096,UInt8} # The cached last error message */
-	internal::Bool				# true if session was initiated by gmt.c
-	deep_debug::Bool			# temporary for debugging
-	parker_fft_default::Bool    # Used to alter the default in -N FFT settings */
-	#int (*print_func) (FILE *, const char *);	# Pointer to fprintf function (may be reset by external APIs like MEX)
-	pf::Ptr{Cvoid}				# Don't know what to put here, so let it be *void
-	do_not_exit::UInt32			# 0 by default, mieaning it is OK to call exit  (may be reset by external APIs like MEX to call return instead)
-	lib::Ptr{Gmt_libinfo}		# List of shared libs to consider
-	n_shared_libs::UInt32		# How many in lib
+struct GMT_CTRL
+	# Master structure for a GMT invocation.  All internal settings for GMT is accessed here
+	session::Cvoid	# struct GMT_SESSION. Structure with all values that do not change throughout a session
+	init::Cvoid		# struct GMT_INIT. Structure with all values that do not change in a GMT_func call
+	common::Cvoid	# struct GMT_COMMON. Structure with all the common GMT command settings (-R -J ..)
+	current::Cvoid	# struct GMT_CURRENT. Structure with all the GMT items that can change during execution,
+	hidden::GMT_INTERNAL	# struct GMT_INTERNAL. Internal global variables that are not to be changed directly by users
+	PSL::Ptr{GMT_POSTSCRIPT}		# Pointer to the PSL structure [or NULL]
+	parent::Ptr{Cvoid}	# struct GMTAPI_CTRL.Owner of this structure [or NULL]; gives access to the API from functions being passed *GMT only
 end
 =#
+
+## 
+if (GMTver >= v"6.5")
+
+	struct GMT_JULIA_POCKET
+		gwf_dir::Ptr{UInt8}				# In API->gwf_dir. GMT WorkFlow dir (NULL if not running in modern mode). 4GMT.jl */
+		col_type::NTuple{2*64, UInt32};	# Type of column on input and output: Time, geographic, etc. 4GMT.jl */
+	end
+
+	struct GMTAPI_CTRL
+		# Master controller which holds all GMT API related information at run-time for a single session.
+		# Users can run several GMT sessions concurrently; each session requires its own structure.
+		# Use GMTAPI_Create_Session to initialize a new session and GMTAPI_Destroy_Session to end it.
+
+		jl_pocket::GMT_JULIA_POCKET	# ...
+		current_rec::NTuple{2,UInt64}	# Current record number >= 0 in the combined virtual dataset (in and out)
+		n_objects::UInt32			# Number of currently active input and output data objects
+		unique_ID::UInt32			# Used to create unique IDs for duration of session
+		session_ID::UInt32			# ID of this session
+		unique_var_ID::UInt32		# Used to create unique object IDs (grid,dataset, etc) for duration of session
+		current_item::NTuple{2,Cint}	# Array number of current dataset being processed (in and out)
+		pad::UInt32					# Session default for number of rows/cols padding for grids [2]
+		external::UInt32			# 1 if called via external API (MATLAB, Python) [0]
+		runmode::UInt32				# nonzero for GMT modern runmode [0 = classic]
+		shape::Cint                 # GMT_IS_COL_FORMAT (1) if column-major (MATLAB, Fortran), GMT_IS_ROW_FORMAT (0) if row-major
+		leave_grid_scaled::UInt32	# 1 if we dont want to unpack a grid after we packed it for writing [0]
+		n_cores::UInt32             # Number of available cores on this system
+		n_tmp_headers::Cint         # Number of temporarily held table headers
+		terminal_width::Cint        # Width of the terminal
+		verbose::UInt32             # Used until GMT is set up
+		registered::NTuple{2,Bool}	# true if at least one source/destination has been registered (in and out)
+		io_enabled::NTuple{2,Bool}	# true if access has been allowed (in and out)
+		module_input::Bool          # true when we are about to read inputs to the module (command line) */
+		usage::Bool                 # Flag when 1-liner modern mode modules just want usage
+		allow_reuse::Bool           # Flag when get_region_from_data can read a file and not flag it as "used"
+		is_file::Bool               # True if current rec-by-rec i/o is from a physical file
+		cache::Bool                 # true if we want to read a cache file via GDAL
+		no_history::Bool            # true if we want to disable the gmt.history mechanism
+		got_remote_wesn::Bool       # true if we obtained w/e/sn via a remote grid/image with no resolution given
+		use_gridline_registration::Bool       # true if default remote grid registration should be gridline, not pixel
+		use_gridline_registration_warn::Bool  # true if we should warn about the above
+		n_objects_alloc::Csize_t	# Allocation counter for data objects
+		error::Int32				# Error code from latest API call [GMT_OK]
+		last_error::Int32			# Error code from previous API call [GMT_OK]
+		shelf::Int32				# Place to pass hidden values within API
+		log_level::UInt32           # 0 = stderr, 1 = just this module, 2 = set until unset
+		io_mode::NTuple{2,UInt32}	# 1 if access as set, 0 if record-by-record
+		tile_wesn::NTuple{6,Cdouble}  # Original region used when getting tiles (perhaps result of -Roblique -J)
+		tile_inc::Cdouble           # Remote grid increment in degrees
+		tile_reg::UInt8             # Remote grid registration */
+		GMT::Ptr{Cvoid}				# Ptr{GMT_CTRL}, maybe one day. Till than just keep it as Cvoid
+		object::Ptr{Ptr{Cvoid}}		# Ptr{Ptr{GMTAPI_DATA_OBJECT}}. List of registered data objects
+		session_tag::Ptr{UInt8}		# Name tag for this session (or NULL)
+		session_name::Ptr{UInt8}    # Unique name for modern mode session (NULL for classic) */
+		tmp_dir::Ptr{UInt8}         # System tmp_dir (NULL if not found)
+		session_dir::Ptr{UInt8}     # GMT Session dir (NULL if not running in modern mode)
+		gwf_dir::Ptr{UInt8}         # GMT WorkFlow dir (NULL if not running in modern mode)
+		tmp_header::Ptr{Ptr{UInt8}} # Temporary table headers held until we are able to write them to destination */
+		tmp_segmentheader::Ptr{UInt8} # Temporary segment header held until we are able to write it to destination */
+		message::Ptr{UInt8}         # To be allocated by Create_Session and used for messages */
+		error_msg::NTuple{4096,UInt8} # The cached last error message */
+		internal::Bool				# true if session was initiated by gmt.c
+		deep_debug::Bool			# temporary for debugging
+		parker_fft_default::Bool    # Used to alter the default in -N FFT settings */
+		#int (*print_func) (FILE *, const char *);	# Pointer to fprintf function (may be reset by external APIs like MEX)
+		pf::Ptr{Cvoid}				# Don't know what to put here, so let it be *void
+		do_not_exit::UInt32			# 0 by default, meaning it is OK to call exit  (may be reset by external APIs like MEX to call return instead)
+		lib::Ptr{Cvoid}				# Ptr{GMT_LIBINFO}. List of shared libs to consider
+		n_shared_libs::UInt32		# How many in lib
+	end
+end
