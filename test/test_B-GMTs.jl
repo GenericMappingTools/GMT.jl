e3050051e479e5a5a7994b0f146402b323c529fd
Joaquim Luis <jmfluis@gmail.com>
2023-03-26 23:11:50 +0100

    Move the gravfft test inside a (GMTver > v"6.1.1")

--- test/test_B-GMTs.jl
+++ test/test_B-GMTs.jl
@@ -76,11 +76,11 @@
 		println("	GRDGRAVMAG3D")
 		grdgravmag3d("@earth_relief_10m", region=(-12.5,-10,35.5,37.5), density=1700, inc=0.05, pad=0.5, z_level=:b, f=:g, Vd=dbg2)
 		@test_throws ErrorException("Bad data type in option F") grdgravmag3d("@earth_relief_10m", region=(-12,-10,35,37), density=1700, inc=0.05, z_level=:a, F=:a)
-	end
 
-	println("	GRAVFFT")
-	G = grdcut("@earth_relief_10m_g", region=(-12.5,-10,35.5,37.5), V=:q);
-	gravfft(G, density=1700, F=(faa=1,slab=1), Vd=dbg2)
+		println("	GRAVFFT")
+		G = grdcut("@earth_relief_10m_g", region=(-12.5,-10,35.5,37.5), V=:q);
+		gravfft(G, density=1700, F=(faa=1,slab=1), Vd=dbg2)
+	end
 
 	println("	GMTREGRESS")
 	d = [0 5.9 1e3 1; 0.9 5.4 1e3 1.8; 1.8 4.4 5e2 4; 2.6 4.6 8e2 8; 3.3 3.5 2e2 2e1; 4.4 3.7 8e1 2e1; 5.2 2.8 6e1 7e1; 6.1 2.8 2e1 7e1; 6.5 2.4 1.8 1e2; 7.4 1.5 1 5e2];
