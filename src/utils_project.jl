395a3e1243a5140174eaef78278d4fdaa8c91ced
Joaquim <jluis@ualg.pt>
2023-04-24 02:07:02 +0100

    Del a print statement

--- src/utils_project.jl
+++ src/utils_project.jl
@@ -94,7 +94,6 @@ function worldrectangular(GI; proj::String="+proj=vandg +over", pm=0, latlim=:au
 	end
 
 	G = grdcut(G, R=(G.x[pix_x[1]], G.x[pix_x[2]], yb, yt))
-	@show(res)
 	return coast != "none" ? (G, worldrectcoast(proj, res)) : G
 end
 
