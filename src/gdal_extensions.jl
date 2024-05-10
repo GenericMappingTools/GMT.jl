d2bf56e8b29cc99bac9ce849b6580f578986d222
Joaquim <jluis@ualg.pt>
2024-05-11 00:53:06 +0100

    Add a new function to compute areas with geodesic precision.

--- src/gdal_extensions.jl
+++ src/gdal_extensions.jl
@@ -200,6 +200,22 @@ geomarea(D) = helper_geoms_run_fun(geomarea, D, false)
 
 # ---------------------------------------------------------------------------------------------------
 """
+    geodesicarea(geom)
+
+Compute geometry area, considered as a surface on the underlying ellipsoid of the SRS attached to the geometry.
+The returned area will always be in square meters, and assumes that polygon edges describe geodesic lines on the ellipsoid.
+If the geometry' SRS is not a geographic one, geometries are reprojected to the underlying geographic SRS of the geometry' SRS.
+
+### Parameters
+* `geom`: the geometry. This can either be a GDAL AbstractGeometry or a GMTdataset (or vector of it), or a Matrix
+
+Returns the area of the geometry in square meters or a negative value in case of error for unsupported geometry types.
+"""
+geodesicarea(geom::AbstractGeometry) = OGR_G_GeodesicArea(geom.ptr)
+geodesicarea(D) = helper_geoms_run_fun(geodesicarea, D, false)
+
+# ---------------------------------------------------------------------------------------------------
+"""
     geomlength(geom)
 
 ### Parameters
