4b771ec86eb19e87639dd7dc8608c2c593634351
Joaquim <jluis@ualg.pt>
2024-06-13 18:18:47 +0100

    Use the new nested inset schema.

--- src/psbasemap.jl
+++ src/psbasemap.jl
@@ -15,9 +15,9 @@ Parameters
     No plotting is performed. Instead, we determine the geographical coordinates of the polygon
     outline for the (possibly oblique) rectangular map domain. 
 - $(_opt_B)
-- **D** | **inset** | **inset_box** :: [Type => Str]
+- **inset** :: [Type => NamedTuple]
 
-    Draw a simple map insert box on the map. Requires -F.
+    Draw a simple map insert box on the map.
 - **F** | **box** :: [Type => Str]
 
     Without further options, draws a rectangular border around any map insert (D), map scale (L)
@@ -57,10 +57,10 @@ function basemap(; first=true, kwargs...)
 	cmd  = parse_Td(d, cmd)
 	cmd  = parse_Tm(d, cmd)
 	cmd  = parse_L(d, cmd)
-	opt_D = parse_type_anchor(d, "", [:D :inset :inset_box],
-	                          (map=("g", arg2str, 1), outside=("J", nothing, 1), inside=("j", nothing, 1), norm=("n", arg2str, 1), paper=("x", arg2str, 1), anchor=("", arg2str, 2), width="+w", size="+w", justify="+j", offset=("+o", arg2str), save="+s", translate="_+t", units="_+u"), 'j')
-	(!IamModern[1] && opt_D != "") && (cmd *= opt_D)
-	(IamModern[1] && opt_D != "") && @warn("The `inset` option is not available in modern mode. Please use the `inset()` function.")
+	#opt_D = parse_type_anchor(d, "", [:D :inset :inset_box],
+	                        #(map=("g", arg2str, 1), outside=("J", nothing, 1), inside=("j", nothing, 1), norm=("n", arg2str, 1), paper=("x", arg2str, 1), anchor=("", arg2str, 2), width="+w", size="+w", justify="+j", offset=("+o", arg2str), save="+s", translate="_+t", units="_+u"), 'j')
+	#(!IamModern[1] && opt_D != "") && (cmd *= opt_D)
+	#(IamModern[1] && opt_D != "") && @warn("The `inset` option is not available in modern mode. Please use the `inset()` function.")
 	_cmd = finish_PS_nested(d, [gmt_proggy * cmd])
 	CTRL.pocket_d[1] = d		# Store d that may be not empty with members to use in other modules
 	finish_PS_module(d, _cmd, "", K, O, true)
