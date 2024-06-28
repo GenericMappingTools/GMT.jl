bb423b0aae08388adc65aa8c658324df5e292bb2
Joaquim <jluis@ualg.pt>
2024-06-28 20:32:03 +0100

    Comments

--- src/GMT.jl
+++ src/GMT.jl
@@ -74,7 +74,7 @@ const global IamModern   = Vector{Bool}(undef, 1);IamModern[1] = false		# To kno
 const global FirstModern = Vector{Bool}(undef, 1);FirstModern[1] = false	# To know
 const global IamModernBySubplot = Vector{Bool}(undef, 1);	IamModernBySubplot[1] = false	# To know if set in subpot
 const global IamSubplot  = Vector{Bool}(undef, 1);IamSubplot[1]  = false	# To know if we are in subplot mode
-const global IamInset    = [false, false]#Vector{Bool}(undef, 1);IamInset[1]    = false	# To know if we are in Inset mode
+const global IamInset    = [false, false]									# To know if we are in Inset mode
 const global usedConfPar = Vector{Bool}(undef, 1);usedConfPar[1] = false	# Hacky solution for the session's memory trouble
 const global ThemeIsOn   = Vector{Bool}(undef, 1);ThemeIsOn[1] = false		# To know if we have an active plot theme
 const global CONVERT_SYNTAX = Vector{Bool}(undef, 1);CONVERT_SYNTAX[1] = false	# To only convert to hard core GMT syntax (like Vd=2)
