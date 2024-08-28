74909b0a9940bc41e7e0524878cd70a621cd9da7
Joaquim <jluis@ualg.pt>
2024-08-28 22:10:45 +0100

    Add FV tests

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -4,4 +4,6 @@
 	pop = 1e6*[791 856 978 1050 1262 1544 1650 2532 6122 8170 11560];
 	p = GMT.polyfit(year, pop, 5);
 	f = GMT.polyval(p, year);
+
+	GMT.facenorm([0. 0 0; 1 1 0; 2 0 0]) == [0.0, 0.0, -1.0]
 end
