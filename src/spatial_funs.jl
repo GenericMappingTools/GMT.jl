198bc67e99d8f64e493c6532faabc5a023e1d322
Joaquim <jluis@ualg.pt>
2024-12-29 23:43:28 +0000

    Rearrange doc strings of polygonlevels()

--- src/spatial_funs.jl
+++ src/spatial_funs.jl
@@ -1,22 +1,24 @@
 # ---------------------------------------------------------------------------------------------------
 """
-    zvals = polygonlevels(D::GDtype, ids::Vector{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
-or
+    zvals = polygonlevels(D::GDtype, ids::VecOrMat{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
 
-    zvals = polygonlevels(D::GDtype, ids::Matrix{String}, vals::Vector{<:Real}; kw...) -> Vector{Float64}
+Create a vector with `zvals` to use in `plot` and where length(zvals) == length(D)
 
-Creates a vector with `zvals` to use in `plot` and where length(zvals) == length(D)
 The elements of `zvals` are made up from the `vals`.
 
-- `ids`:    is a string Vector or Matrix with the ids (attribute names) of the GMTdataset D.
+### Args
+- `D`:      The data in a vector of GMTdataset.
+- `ids`:    is a string Vector or Matrix with the ids (attribute names) of the GMTdataset `D`.
             If a Matrix (2 columns only) then the `att` bellow must also have the two names (string vector
             with two elements) that will be matched against the two elements of each line of `ids`.
             The idea here is to match two conditions: `att[1] == ids[n,1] && att[2] == ids[n,2]`
 - `vals`:   is a vector with the numbers to be used in plot `level` to color the polygons.
-- `attrib` or `att`: keyword to select which attribute to use when matching with contents of the `ids` strings.
-- `nocase` or `insensitive`: a keyword from `kw`. Perform a case insensitive comparision between the contents of
+
+### Kwargs
+- `attrib` or `att`: Select which attribute to use when matching with contents of the `ids` strings.
+- `nocase` or `insensitive`: Perform a case insensitive comparision between the contents of
                `ids` and the attribute specified with `attrib`. Default compares as case sensistive.
-- `repeat`: keyword to replicate the previously known value until it finds a new segment ID for the case
+- `repeat`: Replicate the previously known value until it finds a new segment ID for the case
             when a polygon have no attributes (may happen for the islands in a country).
 
 Returns a Vector{Float64} with the same length as the number of segments in D. Its content are
