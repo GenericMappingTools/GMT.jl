8c5cdef8aae299e14eecb07059a25d934610b908
Joaquim <jluis@ualg.pt>
2025-01-04 03:32:04 +0000

    Add a first cut of a fillsinks function.

--- src/gdal_tools.jl
+++ src/gdal_tools.jl
@@ -107,20 +107,21 @@ of pixels in the raster sharing a common pixel/cell value. The input may be a gr
 be rather slow as it picks lots of polygons in pixels with slightly different values at transitions between colors.
 Its natural use is to digitize masks images.
 
-### Parameters
+### Args
 - `data`: Input data. It can be a GMTgrid or GMTimage object.
-- `kwargs`:
-  - `min, nmin, npixels or ncells`: The minimum number of cells/pixels for a polygon to be retained.
+
+### Kwargs
+- `min, nmin, npixels or ncells`: The minimum number of cells/pixels for a polygon to be retained.
     Default is 1. This can be set to filter out small polygons.
-  - `min_area`: Minimum area in m2 for a polygon to be retained. This option takes precedence over the one
+- `min_area`: Minimum area in m2 for a polygon to be retained. This option takes precedence over the one
     above that is based in the counting of cells. Note also that this is an approximate value because at this
     point we still don't know exactly the latitudes of the polygons.
-  - `max_area`: Maximum area in m2 for a polygon to be retained.
-  - `simplify`: Apply the Douglas-Peucker line simplification algorithm to the poligons. Provide a tolerence
+- `max_area`: Maximum area in m2 for a polygon to be retained.
+- `simplify`: Apply the Douglas-Peucker line simplification algorithm to the poligons. Provide a tolerence
     in meters. For example: `simplify=0.5`. But be warned that this is a risky option since a too large tolerance
 	can lead to loss of otherwise good polygons. A good rule of thumb is to use the cell size for the tolerance.
 	And in fact that is what we do when using `simplify=:auto`.
-  - `sort`: If true, will sort polygons by pixel count. Default is the order that GDAL decides internally.
+- `sort`: If true, will sort polygons by pixel count. Default is the order that GDAL decides internally.
 """
 function polygonize(data::GItype; gdataset=nothing, kwargs...)
 	d = GMT.KW(kwargs)
--- src/lepto_funs.jl
+++ src/lepto_funs.jl
@@ -77,12 +77,10 @@ function imreconstruct(Iseed::GMTimage{Bool, 2}, Imask::GMTimage{Bool, 2}; conn=
 end
 =#
 
-#r = Ref(tuple(5.0, 2.0, 1.0, 6.0))
-#p = Base.unsafe_convert(Ptr{Float64}, r)
-#u = unsafe_wrap(Array, p, 4)
-
 # ---------------------------------------------------------------------------------------------------
-function imfill(mat::Matrix{<:Real}; conn=4, is_transposed=false)
+"""
+"""
+function imfill(mat::Matrix{<:Real}; conn=4, is_transposed=false, layout="TRBa")
     #if (eltype(I) == Bool)  mask = togglemask(mat)
     #else                    mask = I;
 	#end
@@ -91,12 +89,81 @@ function imfill(mat::Matrix{<:Real}; conn=4, is_transposed=false)
     marker = copy(mask)
 
 	marker[2:end-1, 2:end-1] .= typemin(eltype(mat))
-    I2 = imreconstruct(marker, mask, conn=conn, is_transposed=is_transposed)
+    I2 = imreconstruct(marker, mask, conn=conn, is_transposed=is_transposed, layout=layout)
 	GMT.imcomplement!(I2)
 	I2 = I2[2:end-1, 2:end-1]
 	isa(eltype(mat), Bool) && (I2 = (I2 .!= 0))
 	return I2
 end
 function imfill(I::GMTimage; conn=4)::GMTimage
-	mat2img(imfill(I.image; conn=conn, is_transposed=(GMT.getsize(I) == size(I) && I.layout[2] == 'R')), I)
+	mat2img(imfill(I.image; conn=conn, is_transposed=(GMT.getsize(I) == size(I) && I.layout[2] == 'R'), layout=I.layout), I)
+end
+
+# ---------------------------------------------------------------------------------------------------
+"""
+	fillsinks(G::GMTgrid; conn=4, insitu=false)
+
+Fill sinks in a grid.
+
+This function uses the ``imfill`` function to find how to fill sinks in a grid. But since ``imfill``
+operates on UInt8 matrices only the vertical (z) descrimination of the grid is reduced to 256 levels,
+which is not that much.
+
+### Args
+- `G::GMTgrid`: The input grid to process.
+
+### Kwargs
+- `conn::Int`: Connectivity for sink filling. Default is 4.
+- `insitu::Bool`: If `true`, modify the grid in place. Default is `false`.
+  Alternatively, use the conventional form ``fillsinks!(G; conn=4)``.
+
+### Returns
+- A new `GMTgrid` with sinks filled, unless `insitu` is `true`, in which case the input grid is modified and returned.
+
+### Examples
+```julia
+G = peaks();
+G2 = fillsinks(G)
+viz(G2, shade=true)
+```
+"""
+fillsinks!(G::GMTgrid; conn=4) = fillsinks(G; conn=conn, insitu=true)
+function fillsinks(G::GMTgrid; conn=4, insitu=false)
+	I = imagesc(G)
+	I2 = imfill(I, conn=conn)
+	d = (I .== I2')
+	D = polygonize(d)
+	means = isa(D, Vector) ? median.(grdtrack(G, D, o=2)) : median(grdtrack(G, D, o=2))	# The mean of each interpolated contour
+	_G = (insitu == 1) ? G : deepcopy(G)
+
+	function filled_ranges(G, D)
+		x0, y0, dx, dy = G.range[1], G.range[3], G.inc[1], G.inc[2]
+		col_1 = round(Int, (D.bbox[1] - x0) / dx) + 1
+		col_2 = round(Int, (D.bbox[2] - x0) / dx) + 1
+		row_1 = round(Int, (D.bbox[3] - y0) / dy) + 1
+		row_2 = round(Int, (D.bbox[4] - y0) / dy) + 1
+		return x0, y0, dx, dy, col_1, col_2, row_1, row_2
+	end
+
+	if (isa(D, Vector))
+		for k = 1:numel(D)
+			x0, y0, dx, dy, col_1, col_2, row_1, row_2 = filled_ranges(G, D[k])
+			Threads.@threads for i = col_1:col_2
+				t = (i-1)*dx+x0
+				for j = row_1:row_2
+					@inbounds pip(t, (j-1)*dy+y0, D[k].data) >= 0 && (_G.z[j,i] = means[k])
+				end
+			end
+		end
+	else
+		x0, y0, dx, dy, col_1, col_2, row_1, row_2 = filled_ranges(G, D)
+		Threads.@threads for i = col_1:col_2
+			t = (i-1)*dx+x0
+			for j = row_1:row_2
+				@inbounds pip(t, (j-1)*dy+y0, D.data) >= 0 && (_G.z[j,i] = means)
+			end
+		end
+	end
+	_G.range[5] = minimum_nan(_G.z)
+	return _G
 end
