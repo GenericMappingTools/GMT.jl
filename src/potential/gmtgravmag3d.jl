d0c00e75191b99e3829299b8a31fde56603e0f02
Joaquim <jluis@ualg.pt>
2025-01-23 11:57:27 +0000

    Improve gravmag3d code parsing and use new fixes done in GMT lib.

--- src/potential/gmtgravmag3d.jl
+++ src/potential/gmtgravmag3d.jl
@@ -1,5 +1,5 @@
 """
-	gmtgravmag3d(cmd0::String=""; kwargs...)
+	gravmag3d(cmd0::String=""; kwargs...)
 
 Compute the gravity/magnetic anomaly of a 3-D body by the method of Okabe.
 
@@ -32,9 +32,9 @@ Parameters
 - **S** | **radius** :: [Type => Number]
 
     Set search radius in km (valid only in the two grids mode OR when `thickness`) [Default = 30 km].
-- **Tv** | **index** :: [Type => Str]
-- **Tr** | **raw_triang** :: [Type => Str]
-- **Ts** | **stl** :: [Type => Str]
+- **T+v** | **index** :: [Type => Str]
+- **T+r** | **raw_triang** :: [Type => Str]
+- **T+s** | **stl** :: [Type => Str]
 
     Gives names of a xyz and vertex (ndex="vert_file") files defining a close surface.
 - **Z** | **z_level** | **reference_level** :: [Type => Number]
@@ -47,39 +47,62 @@ Parameters
 	imshow(G)
 ```
 """
-function gmtgravmag3d(cmd0::String="", arg1=nothing; kwargs...)
+#
+gravmag3d(cmd0::String; kwargs...) = gravmag3d_helper(cmd0, nothing; kwargs...)
+gravmag3d(arg1; kwargs...)         = gravmag3d_helper("", arg1; kwargs...)
+gravmag3d(; kwargs...)             = gravmag3d_helper("", nothing; kwargs...)
 
+function gravmag3d_helper(cmd0::String, arg1; kwargs...)
 	d = init_module(false, kwargs...)[1]		# Also checks if the user wants ONLY the HELP mode
+	gravmag3d_helper(cmd0, arg1, d)
+end
+#
+
+# ---------------------------------------------------------------------------------------------------
+function gravmag3d_helper(cmd0::String, arg1, d::Dict{Symbol,Any})
 	
 	cmd = parse_common_opts(d, "", [:G :RIr :V_params :bi :f])[1]
-	cmd = parse_these_opts(cmd, d, [[:C :density], [:E :thickness], [:F :track], [:G :grid :outgrid],
+	cmd = parse_these_opts(cmd, d, [[:C :density], [:E :thickness], [:G :grid :outgrid],
 	                                [:L :z_obs :observation_level], [:S :radius], [:Z :reference_level]])
 	cmd = add_opt(d, cmd, "H", [:H :mag_params], (field_dec="", field_dip="", mag="", mag_dec="", mag_dip=""))
 
-	arg2 = nothing;
+	arg2, arg3 = nothing, nothing;
 	if ((val = find_in_dict(d, [:Tv :index], true, "GMTdataset | Mx3 array | String")[1]) !== nothing && val != "")  opt = " -Tv"
 	elseif ((val = find_in_dict(d, [:Tr :raw_triang], true, "GMTdataset | Mx3 array | String")[1]) !== nothing && val != "") opt = " -Tr"
 	elseif ((val = find_in_dict(d, [:Ts :stl :STL], true, "String (file name)")[1]) !== nothing && val != "")  opt = " -Ts"
 	elseif ((val = find_in_dict(d, [:M :body], false, "String | NamedTuple")[1]) !== nothing && val != "")
-		cmd = add_opt(d, cmd, "M", [:M :body], (shape="+s", params=","))
+		if (isa(val, Tuple))
+			cmd = add_opt(Dict(:body => val[1]), cmd, "M", [:M :body], (shape="+s", params=","))
+			for n = 2:numel(val)
+				cmd = add_opt(Dict(:body => val[n]), cmd, "", [:M :body], (shape="+s", params=","))
+			end
+			delete!(d, [:M :body])
+		else
+			cmd = add_opt(d, cmd, "M", [:M :body], (shape="+s", params=","))
+		end
 		opt = " -Ts"
 	elseif (SHOW_KWARGS[1])  opt = "" 
 	else   error("Missing one of 'index', 'raw_triang' or 'str' data")
 	end
 
-	if (opt != " -Ts")		# The STL format can only be requested via file
-		cmd *= opt
+	if (opt != " -Ts")		# The STL format can only be requested via file (so we can keep testing that with old syntax)
 		if (isa(val, Array{<:Real}) || isa(val, GDtype))
 			(arg1 === nothing) ? arg1 = val : arg2 = val		# Find the free slot
+			cmd *= " -T+" * opt[end]	# New -T syntax
 		else
 			cmd *= arg2str(val)
-		end
-		if (opt == " -Tv")
-			(!occursin(" -I", cmd)) && error("For grid output MUST specify grid increment ('I' or 'inc')")
+			cmd *= opt					# Old syntax. This should be removed when we rebuild GMT_jll
 		end
 	end
+
+	if ((val = find_in_dict(d, [:F :track], true, "GMTdataset | Mx2 array | String")[1]) !== nothing && val != "")
+		cmd *= " -F"
+		isa(val, String) ? (cmd *= val) : (arg1 === nothing) ? arg1 = val : (arg2 === nothing) ? arg2 = val : arg3 = val
+	end
+
 	(!occursin(" -F", cmd) && !occursin(" -G", cmd)) && (cmd *= " -G")
 
-	(opt != " -Tv") && return finish_PS_module(d, "gmtgravmag3d " * cmd, "", true, false, false, arg1, arg2)
-	common_grd(d, cmd0, cmd, "gmtgravmag3d ", arg1, arg2)		# Finish build cmd and run it
+	finish_PS_module(d, "gmtgravmag3d " * cmd, "", true, false, false, arg1, arg2, arg3)
 end
+
+const gmtgravmag3d = gravmag3d		# Alias
