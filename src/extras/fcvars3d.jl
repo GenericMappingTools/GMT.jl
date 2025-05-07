5580df1936e465247f1a86aa3d581430d1d9eae0
Joaquim <jluis@ualg.pt>
2025-05-07 20:25:31 +0100

    Rename the era5 to ecmwf and add a method to download forecast data.

---
+++ src/extras/fcvars3d.jl
@@ -0,0 +1,12 @@
+# From https://github.com/ecmwf/ecmwf-opendata
+Dict(
+"d"  => ["Divergence", "s-1"],
+"gh" => ["Geopotential height", "gpm"],
+"q"  => ["Specific humidity", "kg kg-1"],
+"r"  => ["Relative humidity", "%"],
+"t"  => ["Temperature", "K"],
+"u"  => ["U component of wind", "m s-1"],
+"v"  => ["V component of wind", "m s-1"],
+"vo" => ["Vorticity (relative)", "s-1"],
+"w"  => ["Vertical velocity", "m s-1"]
+)
