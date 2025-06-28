191613a461863df4027c38569f32f84f0064e1b4
Joaquim <jluis@ualg.pt>
2025-06-28 21:28:54 +0100

    Reset the double slash after 'vsigzip'

--- src/extras/weather.jl
+++ src/extras/weather.jl
@@ -875,6 +875,6 @@ function load_meteostat(ID::AbstractString, granularity::String, year::Int, verb
 		end
 	end
 	verbose && @info "Reading $fname"
-	fname = Sys.iswindows() ? "/vsigzip/" * fname : "/vsigzip" * fname	# Because unix path alsready has a leading slash
+	fname = Sys.iswindows() ? "/vsigzip/" * fname : "/vsigzip/" * fname	# Because unix path already has a leading slash
 	gdalread(fname)
 end
