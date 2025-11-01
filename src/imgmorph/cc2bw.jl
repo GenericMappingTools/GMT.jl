87f85e6e15f31d4e75102ffdf0def60760624b57
Joaquim <jluis@ualg.pt>
2025-11-01 20:41:18 +0000

    Fix several issues (but not all) with bwconncomp and image layouts. (#1837)

--- src/imgmorph/cc2bw.jl
+++ src/imgmorph/cc2bw.jl
@@ -1,13 +1,17 @@
 """
+    BW = cc2bw(cc::GMTConComp; obj2keep::Union{Int, Vector{Int}}=0)
+
+Convert connected components to binary image
 """
-function cc2bw(cc::GMTConComp)
+function cc2bw(cc::GMTConComp; obj2keep::Union{Int, Vector{Int}}=0)
 	# Create a binary image of the same size as the original
 	I = mat2img(zeros(Bool, cc.image_size), x=cc.x, y=cc.y, inc=cc.inc, layout=cc.layout,
 	            is_transposed=(cc.layout[2] == 'R'), proj4=cc.proj4, wkt=cc.wkt, epsg=cc.epsg)
 	
 	# Mark pixels belonging to any connected component as true
-	for pixel_list in cc.pixel_list
-		for rc in pixel_list
+	plist = (obj2keep == 0) ? (1:cc.num_objects) : isvector(obj2keep) ? obj2keep : (obj2keep:obj2keep)
+	for k = 1:numel(plist)
+		for rc in cc.pixel_list[plist[k]]
 			I[rc] = true
 		end
 	end
