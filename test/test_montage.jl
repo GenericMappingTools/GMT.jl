0d3bc8168976d54d6f9c1502b7d5b2e21507b1c8
Joaquim <jluis@ualg.pt>
2026-02-16 02:54:40 +0000

    Qualify the calls to subplot_panel_sizes

--- test/test_montage.jl
+++ test/test_montage.jl
@@ -15,7 +15,7 @@
 	# --- subplot_panel_sizes (Matrix method) ---
 	# All equal aspect ratios => equal widths and heights
 	a = ones(2, 3)
-	w, h = subplot_panel_sizes(a, total_width=12.0)
+	w, h = GMT.subplot_panel_sizes(a, total_width=12.0)
 	@test length(w) == 3
 	@test length(h) == 2
 	@test isapprox(sum(w), 12.0, atol=0.1)
@@ -26,19 +26,19 @@
 	# Wide vs narrow columns
 	a = [2.0 0.5;
 	     2.0 0.5]
-	w, h = subplot_panel_sizes(a, total_width=20.0)
+	w, h = GMT.subplot_panel_sizes(a, total_width=20.0)
 	@test w[1] > w[2]	# First column should be wider (landscape panels)
 
 	# Tall vs short rows
 	a = [1.0 1.0;
 	     0.5 0.5]
-	w, h = subplot_panel_sizes(a, total_width=20.0)
+	w, h = GMT.subplot_panel_sizes(a, total_width=20.0)
 	@test h[2] > h[1]	# Second row should be taller (portrait panels)
 
 	# With NaN entries (empty panels)
 	a = [1.5 1.0;
 	     NaN 0.8]
-	w, h = subplot_panel_sizes(a, total_width=20.0)
+	w, h = GMT.subplot_panel_sizes(a, total_width=20.0)
 	@test length(w) == 2
 	@test length(h) == 2
 	@test all(isfinite, w)
@@ -46,7 +46,7 @@
 
 	# Single panel
 	a = reshape([1.5], 1, 1)
-	w, h = subplot_panel_sizes(a, total_width=15.0)
+	w, h = GMT.subplot_panel_sizes(a, total_width=15.0)
 	@test isapprox(w[1], 15.0, atol=0.01)
 
 	# --- _fill_nans_ar! ---
@@ -75,7 +75,7 @@
 	montage([I1, I2, I3, I4], grid=(1, 2), panels_size=5, indices=[1, 3], show=false)
 
 	# --- subplot_panel_sizes with GMTimage vector ---
-	w, h = subplot_panel_sizes([I1, I2], ncols=2, total_width=20.0)
+	w, h = GMT.subplot_panel_sizes([I1, I2], ncols=2, total_width=20.0)
 	@test length(w) == 2
 	@test length(h) == 1
 	@test isapprox(sum(w), 20.0, atol=0.1)
